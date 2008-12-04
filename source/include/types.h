af9032015b7ef7b821e2857de8a44cbf45b65feb
yingfeng <newpeak@localhost.localdomain>
2008-12-04 15:25:12 +0800

    directory structure

--- source/include/types.h
+++ source/include/types.h
@@ -4,6 +4,7 @@
 #ifndef WIN32
 #include <stddef.h>
 #include <sys/types.h>
+#include <stdint.h>
 #else
 #include <sys/types.h>
 #include <wchar.h>
@@ -17,5 +18,17 @@ typedef unsigned long		uint32_t;
 typedef unsigned __int64	uint64_t;
 
 #endif //end of WIN32
+
+#define NAMESPACE_IZENELIB_START namespace izenelib{
+#define NAMESPACE_IZENELIB_END }
+
+#define NAMESPACE_IZENELIB_AM_START namespace izenelib{ namespace am{
+#define NAMESPACE_IZENELIB_AM_END }}
+
+#define NAMESPACE_IZENELIB_IR_START namespace izenelib{ namespace ir{
+#define NAMESPACE_IZENELIB_IR_END }}
+
+#define NAMESPACE_IZENELIB_UTIL_START namespace izenelib{ namespace util{
+#define NAMESPACE_IZENELIB_UTIL_END }}
 
 #endif // end of TYPES_H
