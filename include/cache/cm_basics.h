54264636366c272a69ba6f9c1c80a0c5a4a603ee
peisheng.wang <peisheng.wang@izenesoft.com>
2009-04-14 16:37:50 +0800

    wps
    make MLRUCache use cccr_hash

--- include/cache/cm_basics.h
+++ include/cache/cm_basics.h
@@ -14,6 +14,7 @@
 #include <am/fromylib/LinearHashTable.h>
 #include <am/cccr_string_hash_table/cccr_str_hash_table.hpp>
 #include <am/sdb_hash/sdb_hash.h>
+#include <am/cccr_hash/cccr_hash.h>
 #include <util/ProcMemInfo.h>
 
 using namespace izenelib::util;
@@ -65,6 +66,7 @@ template<class T> struct HashFun {
 }
 
 }
+
 /*
 namespace boost {
 namespace serialization {
