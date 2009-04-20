c2efb33d5fed409141e5c0e95a2e350f9f171024
peisheng.wang <peisheng.wang@izenesoft.com>
2009-04-20 18:23:35 +0800

    @wps

---
+++ source/util/hashFunction.cpp
@@ -0,0 +1,63 @@
+#include <util/hashFunction>
+
+namespace izenelib{
+namespace util{
+
+inline uint32_t sdb_hash_fun(const void* kbuf, const size_t ksize) {
+	uint32_t convkey = 0;
+	const char* str = (const char*)kbuf;
+	for (size_t i = 0; i < ksize; i++)
+		convkey = 37*convkey + (uint8_t)*str++;
+	return convkey;
+}
+
+
+uint32_t SuperFastHash (const char * data, int len)
+{
+uint32_t hash = len, tmp;
+int rem;
+
+    if (len <= 0 || data == NULL) return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    /* Main loop */
+    for (;len > 0; len--) {
+        hash  += get16bits (data);
+        tmp    = (get16bits (data+2) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        data  += 2*sizeof (uint16_t);
+        hash  += hash >> 11;
+    }
+
+    /* Handle end cases */
+    switch (rem) {
+        case 3: hash += get16bits (data);
+                hash ^= hash << 16;
+                hash ^= data[sizeof (uint16_t)] << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += get16bits (data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += *data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
+}
+
+}
+}
+
