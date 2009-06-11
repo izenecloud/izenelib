4cabec1e0600d060a25a3db7cfa084cabb8e34b9
Ian Yang <doit.ian@gmail.com>
2009-06-11 15:51:29 +0800

    add more detailed messages and refine dependencies check.

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -11,17 +11,23 @@ INCLUDE(CheckIncludeFile)
 # set BOOST_ROOT to select a boost installation
 
 # This is required to enable the new version
-SET (Boost_ADDITIONAL_VERSIONS 1.37 1.37.0 1.38 1.38.0 1.39 1.39.0)
+SET(Boost_ADDITIONAL_VERSIONS 1.37 1.37.0 1.38 1.38.0 1.39 1.39.0)
 # find unit_test_framework in boost >= 1.35, it's optional and only
 # used in unit testcases
-FIND_PACKAGE (Boost 1.35.0 COMPONENTS unit_test_framework)
+FIND_PACKAGE(Boost 1.35.0 COMPONENTS unit_test_framework)
 # require header only libraries >= 1.35
-find_package (Boost 1.35.0 REQUIRED)
+find_package(Boost 1.35.0 REQUIRED)
+
+##################################################
+# bzip2
+#####
+
+FIND_PACKAGE(BZip2)
 
 ##################################################
 # Tokoyo Cabnet
 #####
-FIND_PACKAGE (TokyoCabinet 1.4.24)
+FIND_PACKAGE(TokyoCabinet 1.4.24)
 
 ##################################################
 # Int Types
@@ -35,12 +41,12 @@ CHECK_INCLUDE_FILE(stddef.h HAVE_STDDEF_H)
 ##################################################
 # Doxygen
 #####
-find_package (Doxygen)
-if (DOXYGEN_DOT_EXECUTABLE)
-  option (USE_DOT "use dot in doxygen?" FLASE)
-endif (DOXYGEN_DOT_EXECUTABLE)
+FIND_PACKAGE(Doxygen)
+IF(DOXYGEN_DOT_EXECUTABLE)
+  OPTION(USE_DOT "use dot in doxygen?" FLASE)
+ENDIF(DOXYGEN_DOT_EXECUTABLE)
 
-set (USE_DOT_YESNO NO)
-if (USE_DOT)
-  set (USE_DOT_YESNO YES)
-endif (USE_DOT)
+SET(USE_DOT_YESNO NO)
+IF(USE_DOT)
+  SET(USE_DOT_YESNO YES)
+ENDIF(USE_DOT)
