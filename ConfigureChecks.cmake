b5570f7e71b05ac03f09a0437bc35c557ca464a6
Ian Yang <doit.ian@gmail.com>
2009-06-12 18:26:16 +0800

    check header file sys/stat.h

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -9,6 +9,7 @@ INCLUDE(CheckIncludeFile)
 CHECK_INCLUDE_FILE(inttypes.h HAVE_INTTYPES_H)
 CHECK_INCLUDE_FILE(stdint.h HAVE_STDINT_H)
 CHECK_INCLUDE_FILE(sys/types.h HAVE_SYS_TYPES_H)
+CHECK_INCLUDE_FILE(sys/stat.h HAVE_SYS_STAT_H)
 CHECK_INCLUDE_FILE(stddef.h HAVE_STDDEF_H)
 
 ##################################################
@@ -35,8 +36,6 @@ ENDIF(USE_DOT)
 SET(Boost_ADDITIONAL_VERSIONS 1.39 1.39.0 1.38 1.38.0 1.37 1.37.0)
 # find unit_test_framework in boost >= 1.35, it's optional and only
 # used in unit testcases
-FIND_PACKAGE(Boost 1.35.0 COMPONENTS unit_test_framework)
-# require header only libraries >= 1.35
 FIND_PACKAGE(Boost 1.35.0 REQUIRED)
 
 ##################################################
