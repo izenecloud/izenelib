0dbca95406c086590b2b09341f6edf54c3563420
peisheng.wang <peisheng.wang@izenesoft.com>
2009-06-16 18:05:20 +0800

    Add test-data for id-manager

--- include/util/detail/izene_serialization_boost.h
+++ include/util/detail/izene_serialization_boost.h
@@ -18,6 +18,7 @@ NS_IZENELIB_UTIL_BEGIN
 
 const int archive_flags = archive::no_header | archive::no_codecvt;
 
+
 template<typename T> class izene_serialization_boost {
 	size_t size_;
 	izene_streambuf b;
@@ -53,6 +54,45 @@ public:
 		ia & dat;
 	}
 };
+
+/*
+
+template<typename T> class izene_serialization_boost {
+	size_t size_;
+	izene_streambuf b;
+	ostream ostr;
+
+public:
+	izene_serialization_boost(const T& dat) :
+		ostr(&b) {
+		{
+			boost::archive::text_oarchive oa(ostr, archive_flags);
+			oa & dat;
+		}
+		size_ = ((izene_streambuf *)ostr.rdbuf() )->size();
+	}
+	~izene_serialization_boost() {
+	}
+	void write_image(char * &ptr, size_t& size) {
+		ptr = ((izene_streambuf *)ostr.rdbuf() )->data();
+		size = size_;
+	}
+};
+
+template<typename T> class izene_deserialization_boost {
+	stringbuf b;
+	istream istr;
+public:
+	izene_deserialization_boost(const char* ptr, const size_t size) :
+		istr(&b) {
+		istr.rdbuf()->pubsetbuf((char* )ptr, size);
+	}
+	void read_image(T& dat) {
+		boost::archive::text_iarchive ia(istr, archive_flags);
+		ia & dat;
+	}
+};*/
+
 
 NS_IZENELIB_UTIL_END
 
