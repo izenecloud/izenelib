682c8bccd7e467e6aef683cb44e0fc8c88d9e70d
peisheng.wang <peisheng.wang@izenesoft.com>
2009-08-18 18:03:51 +0800

    add izene_log.cpp, prevent multipl reference

--- include/util/izene_log.h
+++ include/util/izene_log.h
@@ -21,45 +21,12 @@
 #define DEBUG_NEW new
 #endif
 
-
 using namespace std;
-
 
 NS_IZENELIB_UTIL_BEGIN
 
-string  getMemInfo() {
-	std::stringstream ss;
-	unsigned long rlimit;
-	static unsigned long  vm = 0, rss;
-	unsigned long pre_vm = vm;
-	unsigned long pre_rss = rss;
-	
-	ProcMemInfo::getProcMemInfo(vm, rss, rlimit);
-	ss << "Current vm(virtual memory): " << vm << " bytes; \t rss(Resident Set Size): " << rss << " bytes. " ;
-	if(vm >= pre_vm )
-		ss << "\t ++++ vm: " << vm - pre_vm << " bytes;";				
-	else 
-		ss << "\t ---- vm: " <<  pre_vm - vm << " bytes;";
-		
-	if( rss >= pre_rss )
-		ss <<" \t ++++ rss: " << rss - pre_rss  << " bytes.";
-	else
-		ss <<" \t ---- rss: " << pre_rss - rss  << " bytes.";
-	ss <<endl;	
-	
-	return ss.str();
-}
-
-
-string getProfilingInfo(){
-	std::stringstream ss;
-#ifdef SF1_TIME_CHECK
-	REPORT_PROFILE_TO_SS( ss );
-	return "\n\n" + ss.str() + "\n\n";
-#else
-	return "SF1_TIME_CHECK not defined!";
-#endif
-}
+string  getMemInfo(); 
+string getProfilingInfo();
 
 
 NS_IZENELIB_UTIL_END
