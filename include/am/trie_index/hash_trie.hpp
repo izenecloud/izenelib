269f92f295c43bc6b350c11b96c55a605a24d3a4
Kevin Hu <kevinhu.sh@gmail.com>
2009-08-22 16:09:55 +0800

    trie-index added

---
+++ include/am/trie_index/hash_trie.hpp
@@ -0,0 +1,335 @@
+#ifndef HASH_TRIE
+#define HASH_TRIE
+
+#include "term_hash_table.hpp"
+#include <vector>
+#include <string>
+#include <math.h>
+
+NS_IZENELIB_AM_BEGIN
+
+template<
+  uint32_t ENTRY_SIZE = 1000000,
+  uint32_t VALUE_CACHE_SIZE = 500//MB
+  >
+class HashTrie
+{
+#define NODE_ENTRY_SIZE_TABLE_SIZE 4
+
+  TermHashTable *root_;
+  TermPropertyVector<VALUE_CACHE_SIZE>*   value_pool_;
+  TermHashTable* buf_node_;
+  FILE* f_;
+  uint32_t node_entry_size_[NODE_ENTRY_SIZE_TABLE_SIZE];
+  uint32_t node_num_;
+  
+  void insert_(const std::vector<std::string>& vs, std::size_t start,  uint32_t docid)
+  {
+    TermHashTable* node = root_;
+    for (std::size_t i=start; i<vs.size(); ++i)
+    {
+      uint64_t value1 = value_pool_->touch_append();
+      uint64_t value2 = node->insert(vs[i].c_str(), vs[i].length(), value1);
+      if (value1 == value2)//new term
+      {
+        if (i == vs.size()-1)//the last one
+        {
+          value_pool_->append(1);
+          continue;
+        }
+        
+        node = new TermHashTable(node_entry_size_[i-start+1>=NODE_ENTRY_SIZE_TABLE_SIZE? NODE_ENTRY_SIZE_TABLE_SIZE-1: i-start+1]);
+        ++node_num_;
+        assert(value_pool_->append(1, 1, (uint64_t)node)==value1);
+        continue;
+      }
+
+      value_pool_->add_freq(value2);
+
+      if (i == vs.size()-1)//the last one
+      {
+        continue;
+      }
+      
+      node = (TermHashTable*)value_pool_->get_child(value2);
+      if ((uint64_t)node == (uint64_t)-1)
+      {
+        node = new TermHashTable(node_entry_size_[i-start+1>=NODE_ENTRY_SIZE_TABLE_SIZE? NODE_ENTRY_SIZE_TABLE_SIZE-1: i-start+1]);
+        ++node_num_;
+        value_pool_->set_child(value2, (uint64_t)node);
+        value_pool_->set_loaded(value2, 1);
+      }
+    }
+  }
+
+  uint64_t size_(TermHashTable* tb)
+  {
+    uint64_t r= tb->size();
+    ++node_num_;
+
+    //std::cout<<"*****************\n";
+    //std::cout<<*tb<<std::endl;
+    //std::cout<<tb->size()<<std::endl;
+    
+    for (TermHashTable::const_iterator i=tb->begin();i!=tb->end();++i)
+    {
+      uint64_t value = (*i).value();
+      //std::cout<<(*i).term()<<std::endl;
+      
+      if (!value_pool_->get_loaded(value) || value_pool_->get_child(value)==(uint64_t)-1)
+        continue;
+      r += size_((TermHashTable*)(value_pool_->get_child(value)));
+    }
+    //std::cout<<"-----------------\n";
+    
+    return r;
+  }
+
+  void release_(TermHashTable* tb)
+  {
+    if (tb == NULL)
+      return;
+    
+    for (TermHashTable::const_iterator i=tb->begin();i!=tb->end();++i)
+    {
+      uint64_t value = (*i).value();
+      //std::cout<<(*i).term()<<std::endl;
+      
+      if (!value_pool_->get_loaded(value) || value_pool_->get_child(value)==(uint64_t)-1)
+        continue;
+      release_((TermHashTable*)(value_pool_->get_child(value)));
+    }
+
+    delete tb;
+    ++node_num_;
+  }
+
+  void save_(TermHashTable* tb)
+  {
+    tb->save(f_);
+    
+    for (TermHashTable::const_iterator i=tb->begin();i!=tb->end();++i)
+    {
+      uint64_t value = (*i).value();
+      //std::cout<<(*i).term()<<std::endl;
+      
+      if (!value_pool_->get_loaded(value) || value_pool_->get_child(value)==(uint64_t)-1)
+        continue;
+
+      uint64_t pos = ftell(f_);
+      save_((TermHashTable*)(value_pool_->get_child(value)));
+      value_pool_->set_child(value, pos);
+      value_pool_->set_loaded(value, 0);
+    }
+
+    delete tb;
+  }
+  
+  void load_(TermHashTable* tb, uint64_t addr)
+  {
+    tb->load(f_, addr);
+    
+    for (TermHashTable::const_iterator i=tb->begin();i!=tb->end();++i)
+    {
+      uint64_t value = (*i).value();
+      //std::cout<<(*i).term()<<" "<<value<<std::endl;
+
+      uint64_t child = value_pool_->get_child(value);
+      
+      if (value_pool_->get_loaded(value) || child ==(uint64_t)-1)
+        continue;
+
+      TermHashTable* tht = new TermHashTable();
+      ++node_num_;
+      load_(tht, child);
+      value_pool_->set_child(value, (uint64_t)tht);
+      value_pool_->set_loaded(value, 1);
+    }
+  }
+
+  
+  inline void load()
+  {
+    uint32_t nn = 0;
+    node_num_ = 1;
+    
+    fseek(f_, 0, SEEK_SET);
+    
+    assert(fread(&nn, sizeof(uint32_t), 1, f_)==1);
+    
+    root_ = new TermHashTable(ENTRY_SIZE);
+    load_(root_, ftell(f_));
+    assert(node_num_ == nn);
+
+    //std::cout<<"node num loaded: "<<node_num_<<std::endl;
+  }
+
+  
+public:
+  HashTrie(const char* nm)
+  {
+    node_entry_size_[0] = ENTRY_SIZE;
+    for (uint32_t i=1; i<NODE_ENTRY_SIZE_TABLE_SIZE; ++i)
+    {
+      node_entry_size_[i] = (uint32_t)(sqrt((double)node_entry_size_[i-1])/2+0.5);
+      //std::cout<<node_entry_size_[i]<<std::endl;
+    }
+    
+    buf_node_ = new TermHashTable();
+    
+    std::string s = nm;
+    s += ".val";
+    value_pool_ = new TermPropertyVector<VALUE_CACHE_SIZE>(s.c_str());
+    
+    node_num_ = 1;
+
+    f_ = fopen(nm, "r+");
+    if (f_ == NULL)
+    {
+      f_ = fopen(nm, "w+");
+      assert(fwrite(&node_num_, sizeof(uint32_t), 1, f_)==1);
+      root_ = new TermHashTable(ENTRY_SIZE);
+    }
+    else
+      load();
+    
+    if (f_ == NULL)
+    {
+      std::cout<<"Can't create file: "<<nm<<std::endl;
+      return;
+    }
+  }
+
+  ~HashTrie()
+  {
+    //std::cout<<"deleting trie!\n";
+    fclose(f_);
+    uint32_t nn = node_num_;
+    node_num_ = 0;
+    release_(root_);
+    assert(nn == node_num_);
+    //std::cout<<"deleting value pool!\n";
+    delete value_pool_;
+    delete buf_node_;
+  }
+
+  inline uint32_t node_num()const
+  {
+    return node_num_;
+  }
+  
+  void insert(const std::vector<std::string>& vs, uint32_t docid)
+  {
+    for (std::size_t i=0; i<vs.size(); ++i)
+    {
+      insert_(vs, i, docid);
+    }
+
+    //std::cout<<*root_<<std::endl;
+  }
+
+  uint64_t size()
+  {
+    std::cout<<"Value pool size: "<<value_pool_->size()<<std::endl;
+    node_num_ = 0;
+    return size_(root_)+value_pool_->size();
+  }
+  
+  uint32_t get_freq(const std::vector<std::string>& vs)
+  {
+    TermHashTable* node = root_;
+    for (std::size_t i=0; i<vs.size(); ++i)
+    {
+      uint64_t value = node->find(vs[i].c_str(), vs[i].length());
+      if (value == (uint64_t)-1)
+        return 0;
+
+      if (i == vs.size()-1)//last one
+      {
+        return value_pool_->get_freq(value);
+      }
+      
+      if (value_pool_->get_loaded(value))
+      {
+        node = (TermHashTable* )value_pool_->get_child(value);
+        continue;
+      }
+      node = buf_node_;
+      node->load(f_, value_pool_->get_child(value));
+    }
+
+    return 0;
+  }
+
+  bool get_suffix(const std::vector<std::string>& terms, std::vector<std::string>& suffixs, std::vector<uint32_t>& counts)
+  {
+    suffixs.clear();
+    counts.clear();
+    
+    TermHashTable* node = root_;
+    for (std::size_t i=0; i<terms.size(); ++i)
+    {
+      uint64_t value = node->find(terms[i].c_str(), terms[i].length());
+      if (value == (uint64_t)-1)
+        return false;
+
+      if (i == terms.size()-1)//last one
+      {
+        if (value_pool_->get_loaded(value))
+          node = (TermHashTable* )value_pool_->get_child(value);
+        else
+        {
+          node = buf_node_;
+          node->load(f_, value_pool_->get_child(value));
+        }
+
+        break;
+      }
+      
+      if (value_pool_->get_loaded(value))
+      {
+        node = (TermHashTable* )value_pool_->get_child(value);
+        continue;
+      }
+      node = buf_node_;
+      node->load(f_,value_pool_->get_child(value));
+    }
+    
+    for (TermHashTable::const_iterator i=node->begin(); i!=node->end(); ++i)
+    {
+      uint64_t va = (*i).value();
+      suffixs.push_back(std::string((*i).term()));
+      counts.push_back(value_pool_->get_freq(va));
+    }
+
+    return true;
+  }
+
+  inline void save()
+  {
+    fseek(f_, 0, SEEK_SET);
+    assert(fwrite(&node_num_, sizeof(uint32_t), 1, f_)==1);
+    save_(root_);
+    fflush(f_);
+    root_ = NULL;
+    node_num_ = 0;
+  }
+  
+}
+;
+NS_IZENELIB_AM_END
+#endif
+
+/**
+   TgPhraseExtractor(const std::string& path);
+		~TgPhraseExtractor();
+		void addSentence(const std::vector<termid_t>& termList, const std::vector<loc_t>& positionList, docid_t docId);
+		bool analysis();
+		labelid_t getPhraseId(const std::vector<termid_t>& termList);
+		count_t freq(labelid_t labelId);
+		bool getDocIdList(labelid_t labelId, std::vector<docid_t>& docIdList);
+		private:
+			SuffixNodeList begin();
+			bool getAllPrefix(const std::vector<termid_t>& termList, std::vector<termid_t>& prefixList, std::vector<count_t>& countList);
+			bool getAllSuffix(const std::vector<termid_t>& termList, std::vector<termid_t>& suffixList, std::vector<count_t>& countList);
+ **/
---
+++ include/am/trie_index/term_hash_table1.hpp
@@ -0,0 +1,653 @@
+#ifndef TERM_HASH_TABLE
+#define TERM_HASH_TABLE
+
+#include<types.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include <iostream>
+#include <assert.h>
+
+NS_IZENELIB_AM_BEGIN
+
+
+/**
+   len(1)|freq(4)|loaded(1)|child(8)|doc_list(8)|term\0|....
+ **/
+class Term
+{
+public:
+  typedef uint32_t freq_t;
+  typedef uint64_t termid_t;
+
+private:
+  char* buf_;
+
+public:
+  struct TERM
+  {
+    char id[sizeof(termid_t)];
+    char   freq[sizeof(freq_t)];
+    char loaded;
+    char child[8];
+    char doc_list[8];
+  }
+    ;
+protected:
+  
+#define GET_ID(buf) (*(termid_t*)((TERM*)(buf))->id)
+#define GET_FREQ(buf) (*(freq_t*)((TERM*)(buf))->freq)
+#define GET_LOADED(buf) (((TERM*)(buf))->loaded)
+#define GET_CHILD(buf) (*(uint64_t*)((TERM*)(buf))->child)
+#define GET_DOC_LIST(buf) (*(uint64_t*)((TERM*)(buf))->doc_list)
+
+public:
+  inline explicit Term(char* buf)
+    :buf_(buf)
+  {
+  }
+
+  Term(termid_t id,  char* buf, uint64_t child = -1, uint64_t doc_list = -1)
+  {
+    buf_ = buf;
+    GET_ID(buf_) = id;
+    GET_FREQ(buf_) = 1;
+    GET_LOADED(buf_) = 0;
+    GET_CHILD(buf_) = child;
+    GET_DOC_LIST(buf_) = doc_list;
+    
+  }
+
+  static inline uint32_t need_size()
+  {
+    return sizeof(struct TERM);
+  }
+  
+  static inline uint32_t size()
+  {
+    return sizeof(struct TERM);
+  }
+  
+  inline freq_t frequency()const
+  {
+    return GET_FREQ(buf_);
+  }
+
+  inline bool get_loaded()const
+  {
+    return GET_LOADED(buf_);
+  }
+
+  inline uint64_t get_child()const
+  {
+    return GET_CHILD(buf_);
+  }
+
+  inline uint64_t get_doc_list()const
+  {
+    return GET_DOC_LIST(buf_);
+  }
+
+  inline termid_t get_id() const
+  {
+    return GET_ID(buf_);
+  }
+
+  inline char* next() const
+  {
+    return buf_+sizeof(struct TERM);
+  }
+  
+  bool operator == (const Term& term)
+  {
+    return get_id() == term.get_id();
+  }
+
+  bool operator == (const char* str)
+  {
+    return get_id() == GET_ID(str);
+  }
+  
+  inline void add_freq()
+  {
+    GET_FREQ(buf_)++;
+  }
+
+  inline uint32_t get_freq()const
+  {
+    return GET_FREQ(buf_);
+  }
+
+  inline void set_loaded(bool b)
+  {
+    GET_LOADED(buf_) = b;
+  }
+  
+  inline void set_child(uint64_t child)
+  {
+    GET_CHILD(buf_) = child;
+  }
+
+  inline void set_doc_list(uint64_t list)
+  {
+    GET_DOC_LIST(buf_) = list;
+  }
+
+  
+  /**
+     If it's not equal, return next term, if it is, return current term.
+   **/
+  inline bool is_equal(uint64_t id, char** t)
+  {
+    if (get_id()!=id)
+    {
+      *t = next();
+      return false;
+    }
+
+    *t = (buf_);
+    return true;
+  }
+
+  bool operator != (const Term& other)
+  {
+    return buf_!=other.buf_;
+  }
+
+  inline bool is_null()const
+  {
+    return buf_==NULL;
+  }
+  
+friend std::ostream& operator << (std::ostream& os, const Term& t)
+  {
+    os<<"<"<<GET_ID(t.buf_)<<" "<<GET_FREQ(t.buf_)<<" "<<GET_LOADED(t.buf_)<<" "<<GET_CHILD(t.buf_)<<" "<<GET_DOC_LIST(t.buf_)<<">";
+    
+    return os;
+  }
+
+  
+}  ;
+
+
+/**
+   max_size(4)|size(4)|Term...
+   max_size is the size of the entire buffer ;
+ **/
+class Row
+{
+  struct ROW_HEAD
+  {
+    uint32_t max_size;
+    uint32_t size;
+  }
+    ;
+
+  char* buf_;
+
+#define GET_MAX_SIZE(buf) (((ROW_HEAD*)(buf))->max_size)
+#define GET_SIZE(buf) (((ROW_HEAD*)(buf))->size)
+public:
+  
+  inline Row(Term::termid_t id)
+  {
+    buf_ = (char*)malloc(sizeof (struct ROW_HEAD) + sizeof(struct Term::TERM));
+    GET_MAX_SIZE(buf_) = sizeof (struct ROW_HEAD)+sizeof(struct Term::TERM);
+    GET_SIZE(buf_) = sizeof (struct ROW_HEAD)+sizeof(struct Term::TERM);
+    Term(id, buf_+sizeof(struct ROW_HEAD));
+  }
+
+  
+  inline Row(FILE* f)
+  {
+    uint32_t size = 0;
+    assert(fread(&size, sizeof(uint32_t), 1, f)==1);
+    buf_ = (char*)malloc(size);
+    GET_MAX_SIZE(buf_) = size;
+    assert(fread(buf_+sizeof(uint32_t), size-sizeof(uint32_t), 1, f)==1);
+  }
+
+  inline ~Row()
+  {
+    free(buf_);
+  }
+
+  inline uint32_t max_size()const
+  {
+    return GET_MAX_SIZE(buf_);
+  }
+
+  inline uint32_t size() const
+  {
+    return GET_SIZE(buf_);
+  }
+
+  inline uint32_t count()const
+  {
+    return (GET_SIZE(buf_) - sizeof (struct ROW_HEAD))/sizeof(struct Term::TERM);
+  }
+  
+  inline char* append_term(const Term::termid_t id)
+  {
+    while (max_size()-size()<= Term::need_size())
+    {
+      buf_ = (char*)realloc(buf_, 2*size());
+      GET_MAX_SIZE(buf_) = 2*size();
+    }
+
+    char* r = buf_+size();
+    Term t(id, r);
+    GET_SIZE(buf_) = t.size();//add size
+
+    return r;
+  }
+  
+  char* find(Term::termid_t id)
+  {
+    char* p = buf_ + sizeof(struct ROW_HEAD);
+    uint32_t num = count();
+    
+    for (uint32_t i=0; i<num; ++i)
+    {
+      Term t(p);
+      if (t.is_equal(id, &p))
+        return p;
+    }
+
+    return NULL;
+  }
+
+  char* add_term(const Term::termid_t id, bool& is_new)
+  {
+    is_new = false;
+    
+    char* p = buf_+sizeof(struct ROW_HEAD);
+    uint32_t num = count();
+    for (uint32_t i=0; i<num; ++i)
+    {
+      Term t(p);
+      if (t.get_id() == id)
+      {
+        t.add_freq();
+        return p;
+      }
+      p = t.next();
+    }
+    
+    while (max_size()-size()<= Term::need_size())
+    {
+      buf_ = (char*)realloc(buf_, 2*size());
+      GET_MAX_SIZE(buf_) = 2*size();
+    }
+
+    p = buf_+size();
+    Term t(id, p);
+    GET_SIZE(buf_) += t.size();//add size
+    
+    is_new = true;
+
+    return p;
+    
+  }
+  
+  inline const char* get_buf()const
+  {
+    return buf_;
+  }
+
+  inline char*  begin()const
+  {
+    return (buf_ + sizeof(struct ROW_HEAD));
+  }
+
+  inline void compact()
+  {
+    uint32_t s = size();
+    if (max_size()-s<=1)
+      return;
+
+    buf_ = (char*)realloc(buf_, s);
+    GET_MAX_SIZE(buf_) = s;
+  }
+
+  inline uint32_t save(FILE* f)
+  {
+    uint32_t s = GET_MAX_SIZE(buf_);
+    GET_MAX_SIZE(buf_) = size();
+    assert(fwrite(buf_, size(), 1, f)==1);
+    GET_MAX_SIZE(buf_) = s;
+    return size();
+  }
+
+friend std::ostream& operator << (std::ostream& os, const Row& row)
+  {
+    os<<"\nmax_size:"<<row.max_size()<<"|size:"<<row.size()<<"|count:"<<row.count()<<"\n----------\n";
+
+    Term t(row.buf_+sizeof(struct Row::ROW_HEAD));
+    
+    for (uint32_t i=0; i< row.count(); ++i)
+    {
+      os<<"|"<<t;
+      t = Term(t.next());
+    }
+
+    os<<std::endl;
+    return os;
+  }
+  
+  
+}
+  ;
+
+class TermHashTable
+{
+  uint32_t num_;
+  Row**    entry_;
+  uint32_t entry_size_;
+
+  typedef TermHashTable SelfT;
+
+public:
+  class const_iterator
+  {
+    const Row**    entry_;
+    const uint32_t entry_size_;
+    uint32_t entry_num_;
+    Term t_;
+
+  public:
+    const_iterator(const Row** entry, uint32_t entry_size, uint32_t entry_num)
+      : entry_(entry), entry_size_ (entry_size), entry_num_(entry_num),
+        t_(entry_num< entry_size? Term(entry_[entry_num_]->begin()): Term(0))
+    {
+    }
+
+    
+    const_iterator(const const_iterator& other)
+      : entry_(other.entry_), entry_size_ (other.entry_size_), entry_num_(other.entry_num_), t_(other.t_)
+    {
+    }
+
+    Term& operator *()
+    {
+      return t_;
+    }
+
+    const_iterator& operator++()
+    {
+      //std::cout<<t_<<std::endl;
+      char* p = t_.next();
+      if (p - entry_[entry_num_]->get_buf()>=entry_[entry_num_]->size())
+      {
+        for (++entry_num_; entry_num_<entry_size_; ++entry_num_)
+          if (entry_[entry_num_]!=NULL)
+          {
+            t_ = Term(entry_[entry_num_]->begin());
+            return *this;
+          }
+        
+        t_ = Term(0);
+        return *this;
+      }
+
+      t_ = Term(p);
+      
+      return(*this);
+    }
+
+//     const_iterator operator++(int)
+//     {
+//       const_iterator tmp(*this);
+
+//       char* p = t_.next();
+//       if (p - entry_[entry_num_]->get_buf()>=entry_[entry_num_]->size())
+//       {
+//         for (; entry_num_<entry_size_; ++entry_num_)
+//           if (entry_[entry_num_]!=NULL)
+//           {
+//             t_ = entry_[entry_num_]->begin();
+//             return *this;
+//           }
+//         t_ = Term(0);
+//         return *this;
+//       }
+
+//       t_ = Term(p);
+
+//       return(tmp);
+//     }
+
+    bool operator != (const const_iterator& other)
+    {
+      return entry_ != other.entry_ || entry_size_ != other.entry_size_ || entry_num_ != other.entry_num_ || t_ != other.t_;
+    }
+    
+  };
+
+  const_iterator begin()
+  {
+    for (uint32_t i=0; i<entry_size_; ++i)
+      if (entry_[i]!= NULL)
+        return const_iterator((const Row**)entry_, entry_size_, i);
+
+    return end();
+  }
+
+  const_iterator end()
+  {
+    return const_iterator((const Row**)entry_, entry_size_, entry_size_);
+  }
+  
+    
+  inline TermHashTable(uint32_t entry_size)
+  {
+    num_ = 0;
+    entry_size_ = entry_size;
+    
+    entry_ = (Row**)malloc(entry_size_*sizeof(Row*));//new Row*[entry_size];
+    for (uint32_t i=0;i<entry_size; ++i)
+      entry_[i] = NULL;
+  }
+
+  inline TermHashTable()
+  {
+    num_ = 0;
+    entry_size_ = 0;
+    entry_ = NULL;
+  }
+
+  inline ~TermHashTable()
+  {
+    release();
+    free(entry_);
+  }
+  
+  inline void release()
+  {
+    if (entry_ == NULL)
+      return;
+    
+    for (uint32_t i=0;i<entry_size_; ++i)
+      if(entry_[i] != NULL)
+      {
+        delete entry_[i];
+        entry_[i] = NULL;
+      }
+  }
+
+  uint64_t size()const
+  {
+    uint64_t s = 0;
+    for (uint32_t i=0; i<entry_size_; ++i)
+      if (entry_[i]!=NULL)
+        s += entry_[i]->max_size();
+
+    s+= entry_size_*sizeof(Row*);
+    
+    return s;
+  }
+  
+  char* insert(Term::termid_t id, bool& is_new)
+  {
+    is_new = false;
+    
+    uint32_t convkey = id%entry_size_;
+
+    if (entry_[convkey] == NULL)
+    {
+      entry_[convkey] = new Row(id);
+      is_new = true;
+      ++num_;
+      return entry_[convkey]->begin();
+    }
+
+    char* r = entry_[convkey]->add_term(id, is_new);
+    if (is_new)
+      ++num_;
+    
+    return r;
+  }
+
+  char* find(Term::termid_t id)
+  {
+    uint32_t convkey = id%entry_size_;
+
+    if (entry_[convkey] == NULL)
+      return NULL;
+
+    return entry_[convkey]->find(id);
+
+  }
+  
+  inline void compact()
+  {
+    for (uint32_t i=0; i<entry_size_; ++i)
+      if (entry_[i]!=NULL)
+        entry_[i]->compact();
+  }
+
+  inline uint32_t term_num()const
+  {
+    return num_;
+  }
+  
+  void save(FILE* f)
+  {
+    uint64_t s = 0;
+    uint32_t entry_num = 0;
+    
+    long int pos1 = ftell(f);
+    
+    assert(fwrite(&s, sizeof(uint64_t),1, f)==1);
+    assert(fwrite(&entry_num, sizeof(uint32_t),1, f)==1);
+    assert(fwrite(&entry_size_, sizeof(uint32_t),1, f)==1);
+    assert(fwrite(&num_, sizeof(uint32_t),1, f)==1);
+    
+    for (uint32_t i=0; i<entry_size_; ++i)
+      if (entry_[i]!= NULL)
+      {
+        ++entry_num;
+        assert(fwrite(&i, sizeof(uint32_t),1, f)==1);
+        s += entry_[i]->save(f);
+      }
+
+    long int pos2 = ftell(f);
+    
+    fseek(f, pos1, SEEK_SET);
+    assert(fwrite(&s, sizeof(uint64_t),1, f)==1);
+    assert(fwrite(&entry_num, sizeof(uint32_t),1, f)==1);
+    //std::cout<<s<<std::endl;
+
+    fseek(f, pos2, SEEK_SET);
+  }
+
+//   char* load(char* buf, uint32_t buf_size, FILE* f, uint64_t addr=0)
+//   {
+//     release();
+//     uint64_t s = 0;
+
+//     fseek(f, addr, SEEK_SET);
+//     assert(fread(&s, sizeof(uint64_t),1, f)==1);
+//     if (s > buf_size)
+//       return NULL;
+
+//     uint32_t es = 0;
+//     //entry number
+//     assert(fread(&es, sizeof(uint32_t),1, f)==1);
+//     assert(fread(&es, sizeof(uint32_t),1, f)==1);
+
+//     if (entry_==NULL || es != entry_size_)
+//     {
+//       entry_size_ = es;
+//       entry_ = (Row**)malloc(entry_size_*sizeof(Row*));//entry_ = new Row*[entry_size_];
+//       for (uint32_t i=0;i<entry_size_; ++i)
+//         entry_[i] = NULL;
+//     }
+    
+//     assert(fread(&num_, sizeof(uint32_t),1, f)==1);
+//     assert(fread(buf, s, 1, f)==1);
+
+//     uint64_t size = 0;
+//     while (size<s)
+//     {
+//       Row* row = new Row(buf+size);
+//       entry_[row->index()] = row;
+//       size += row->size();
+//     }
+
+//     return buf;
+//   }
+
+  uint32_t load(FILE* f, uint64_t addr=0)
+  {
+    release();
+    
+    uint64_t s = 0;
+    uint32_t entry_num = 0;
+
+    fseek(f, addr+sizeof(uint64_t), SEEK_SET);
+    
+    assert(fread(&entry_num, sizeof(uint32_t),1, f)==1);
+    //entry size
+    assert(fread(&s, sizeof(uint32_t),1, f)==1);
+    
+    if (s != entry_size_ )
+    {
+      entry_size_ = s;
+      if (entry_ != NULL)
+        delete entry_;
+      entry_ = (Row**)malloc(entry_size_*sizeof(Row*));//entry_ = new Row*[entry_size_];
+      for (uint32_t i=0;i<entry_size_; ++i)
+        entry_[i] = NULL;
+    }
+
+    uint32_t size = 0;
+    assert(fread(&num_, sizeof(uint32_t),1, f)==1);
+    for (uint32_t i=0; i<entry_num; ++i)
+    {
+      uint32_t e = 0;
+      assert(fread(&e, sizeof(uint32_t),1, f)==1);
+      Row* row = new Row(f);
+      entry_[e] = row;
+      size += row->size();
+    }
+    
+    return size;
+  }
+
+friend std::ostream& operator << (std::ostream& os, const SelfT& tb)
+  {
+    for (uint32_t i=0; i<tb.entry_size_; ++i)
+      if (tb.entry_[i]!= NULL)
+      {
+        os<<i<<" ########\n";
+        os<<(*tb.entry_[i]);
+      }
+    return os;
+  }
+  
+}
+  ;
+
+NS_IZENELIB_AM_END
+
+#endif
