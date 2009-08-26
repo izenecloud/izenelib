ea50b7fb3ae2879980406cc7e0d7ad3826637846
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2009-08-26 12:27:20 +0800

    Null run in BTreeIndexer will occupy more than 80% of the total indexing time! It is fixed

--- source/ir/index_manager/index/CollectionIndexer.cpp
+++ source/ir/index_manager/index/CollectionIndexer.cpp
@@ -64,24 +64,29 @@ void CollectionIndexer::addDocument(IndexerDocument* pDoc)
 
     map<IndexerPropertyConfig, IndexerDocumentPropertyType> propertyValueList;
     pDoc->getPropertyList(propertyValueList);
-    pForwardIndexWriter_->addDocument(uniqueID.docId);
 
+    pForwardIndexWriter_->addDocument(uniqueID.docId);
     for (map<IndexerPropertyConfig, IndexerDocumentPropertyType>::iterator iter = propertyValueList.begin(); iter != propertyValueList.end(); ++iter)
     {
-        if (! iter->first.isForward())
+        if(!iter->first.isIndex())
+            continue;
+        if (!iter->first.isForward())
         {
             pIndexer_->getBTreeIndexer()->add(uniqueID.colId, iter->first.getPropertyId(), boost::get<PropertyType>(iter->second), uniqueID.docId);
         }
         else
         {
             map<string, boost::shared_ptr<FieldIndexer> >::iterator it = fieldIndexerMap_.find(iter->first.getName());
+			
             if (it == fieldIndexerMap_.end())
                 // This field is not indexed.
                 continue;
             if(! iter->first.isLAInput())
             {
                 boost::shared_ptr<ForwardIndex> forwardIndex = boost::get<boost::shared_ptr<ForwardIndex> >(iter->second);
+
                 it->second->addField(uniqueID.docId, forwardIndex);
+				
                 pForwardIndexWriter_->addProperty(iter->first.getPropertyId(), forwardIndex);
             }
             else
