035ce4afa90208d091bba8ede7c11ba211eb643e
Kevin Hu <kevinhu.sh@gmail.com>
2009-09-16 11:55:56 +0800

    add graph index

---
+++ include/am/graph_index/dyn_array.hpp
@@ -0,0 +1,816 @@
+#ifndef DYN_ARRAY_HPP
+#define DYN_ARRAY_HPP
+
+#include<types.h>
+#include <vector>
+#include <ostream>
+#include <iostream>
+#include <assert.h>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+NS_IZENELIB_AM_BEGIN
+
+
+template<
+  class VALUE_TYPE = unsigned int,
+  bool  AUTO_SORT = false,
+  uint8_t APPEND_RATE = 1, // POWER OF 2
+  bool COPY_ON_WRITE = true,
+  class LENG_TYPE = uint32_t
+  >
+class DynArray
+{
+  typedef uint8_t  ReferT;
+  typedef DynArray<VALUE_TYPE, AUTO_SORT, APPEND_RATE, COPY_ON_WRITE, LENG_TYPE> SelfT;
+public:
+  typedef LENG_TYPE size_t;
+  static const size_t NOT_FOUND = -1;
+
+#define ARRAY(p) ((VALUE_TYPE*)((p)+sizeof(ReferT)))
+#define REFER (*(ReferT*)p_)
+private:
+  char* p_;
+  size_t length_;
+  size_t max_size_;
+
+protected:
+  inline void refer()
+  {
+    if (!COPY_ON_WRITE)
+      return ;
+        
+    if (p_!=NULL)
+    {
+      if (REFER ==(ReferT)-1)
+      {
+        char* p = (char*)malloc(length_*sizeof(VALUE_TYPE)+sizeof(ReferT));
+        memcpy((char*)ARRAY(p), (char*)ARRAY(p_), size());
+          
+        p_ = p;
+        REFER = 1;
+        return;
+      }
+      (*(ReferT*)p_)++;
+      
+    }
+
+  }
+
+  inline void derefer()
+  {
+    if (!COPY_ON_WRITE)
+    {
+      if (p_!=NULL){
+        free(p_);
+        p_ = NULL;
+      }
+      
+      return ;
+    }
+
+    if (p_!=NULL && REFER > 0)
+    {
+      //boost::mutex::scoped_lock lock(mutex_);
+      //if (p_!=NULL && *(ReferT*)p_ > 0)
+
+      REFER--;
+      if (REFER == 0)
+      {
+        //std::cout<<"free me!\n";
+        free(p_);
+        p_ = NULL;
+        //std::cout<<"alright\n";
+      }
+    }
+
+  }
+
+  inline bool is_refered()const
+  {
+    if (!COPY_ON_WRITE)
+      return false;
+
+    if (p_!= NULL && REFER > 1)
+    {
+        return true;
+    }
+
+    return false;
+  }
+
+  inline void clean_reference()
+  {
+    if (!COPY_ON_WRITE)
+      return ;
+    
+    if (p_!=NULL)
+    {
+      REFER = 1;
+    }
+    
+  }
+
+  inline void assign_self()
+  {
+    //std::cout<<"assign_self............\n";
+    char* p = (char*)malloc(length_*sizeof(VALUE_TYPE)+sizeof(ReferT));
+    max_size_ = length_;
+    memcpy((char*)ARRAY(p), ARRAY(p_), capacity());
+    
+    derefer();
+    p_ = p;
+    clean_reference(); 
+  }
+
+  inline void new_one(size_t t)
+  {
+    length_ = max_size_ = t;
+    p_ = (char*)malloc(t*sizeof(VALUE_TYPE)+sizeof(ReferT));
+    clean_reference();
+  }
+
+  inline void enlarge(size_t t)
+  {
+    max_size_ = t;
+    p_ = (char*)realloc(p_, t*sizeof(VALUE_TYPE)+sizeof(ReferT));
+
+    clean_reference();
+  }
+
+  inline void shrink()
+  {
+    max_size_ = length_;
+    p_ = (char*)realloc(p_, length_*sizeof(VALUE_TYPE)+sizeof(ReferT));
+
+    clean_reference();
+  }
+
+  inline size_t binary_search(VALUE_TYPE t, size_t low, size_t high)const
+  {
+    size_t mid;
+    
+    if (t> ARRAY(p_)[length_-1])
+      return length_-1;
+    if (t < ARRAY(p_)[0])
+      return -1;
+    
+    while (low < high) {
+      mid = low + ((high - low) / 2);  // Note: not (low + high) / 2 !!
+        if (ARRAY(p_)[mid] < t)
+          low = mid + 1; 
+        else
+          //can't be high = mid-1: here A[mid] >= value,
+          //so high can't be < mid if A[mid] == value
+          high = mid; 
+      }
+    // high == low, using high or low depends on taste
+    if (ARRAY(p_)[low]<=t)
+      return low;
+    else
+      return low-1;
+  }
+
+  inline size_t find_pos_2insert(VALUE_TYPE t)const
+  {
+    if (length_ ==0)
+      return -1;
+    
+    if (!AUTO_SORT)
+      return length_-1;
+
+    return binary_search(t, 0, length_-1);
+  }
+
+  
+   /**
+     Swap two elements.
+  **/
+  void swap_(VALUE_TYPE* a, VALUE_TYPE* b)
+  {
+    VALUE_TYPE temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+
+  /**
+     When quick sort, it return the index of media element.
+   **/
+  uint32_t findMedianIndex_(VALUE_TYPE* array, uint32_t left, uint32_t right)
+  {
+    return (left+right)/2;
+    
+//     VALUE_TYPE min = array[left];
+//     VALUE_TYPE max = array[left];
+//     uint32_t shift = 5;
+
+//     if (right-left<2*shift)
+//       return (left+right)/2;
+    
+//     for (uint32_t i=left+shift; i<=right; i+=shift)
+//     {
+//       if (array[i]<min)
+//       {
+//         min = array[i];
+//         continue;
+//       }
+
+//       if (array[i] > max)
+//       {
+//         max = array[i];
+//         continue;
+//       }
+//     }
+
+//     VALUE_TYPE average = (min + max )/2;
+//     uint32_t idx = left;
+//     VALUE_TYPE minGap = average>array[idx]? average-array[idx] : array[idx]-average;
+    
+//     for (uint32_t i=left+shift; i<=right; i+=shift)
+//     {
+//       VALUE_TYPE gap = average>array[i]? average-array[i] : array[i]-average;
+//       if (gap<minGap)
+//       {
+//         minGap = gap;
+//         idx = i;
+//       }
+//     }
+
+//     return idx;
+    
+  }
+  
+  /**
+     Partition the array into two halves and return the index about which the array is partitioned.
+  **/
+  uint32_t partition_(VALUE_TYPE* array, uint32_t left, uint32_t right)
+  {
+    uint32_t pivotIndex = findMedianIndex_(array, left, right), index = left, i;
+    VALUE_TYPE pivotValue = array[pivotIndex];
+ 
+    swap_(&array[pivotIndex], &array[right]);
+    
+    for(i = left; i <=right; i++)
+    {
+      if(array[i] < pivotValue)
+      {
+        swap_(&array[i], &array[index]);
+        index += 1;
+      }
+    }
+    swap_(&array[right], &array[index]);
+ 
+    return index;
+  }
+
+  /**
+     A recursive function applying quick sort.
+   **/
+  void quickSort_(VALUE_TYPE* array, uint32_t left, uint32_t right)
+  {
+    assert(left < right);
+    assert(right < length_);
+    
+    if(right-left<=1)
+    {
+      if (array[left]>array[right])
+        swap_(&array[left], &array[right]);
+      return;
+    }
+    
+    uint32_t idx = partition_(array, left, right);
+    
+    if(idx>0 && left<idx-1)
+      quickSort_(array, left, idx - 1);
+    if (idx+1<length_ && idx+1<right)
+      quickSort_(array, idx + 1, right);
+  }
+
+public:
+  
+  inline explicit DynArray()
+    :p_(NULL), length_(0), max_size_(0)
+  {
+  }
+
+  inline explicit DynArray(const std::vector<VALUE_TYPE>& v)
+    :p_(NULL), length_(0), max_size_(0)
+  {
+    assign(v);
+  }
+
+  DynArray(const char* p, size_t len)
+    :p_(NULL), length_(0), max_size_(0)
+  {
+    assign(p, len);
+  }
+
+  DynArray(const SelfT& other)
+    :p_(NULL), length_(0), max_size_(0)
+  {
+    assign(other);
+  }
+
+  explicit DynArray(size_t n)
+    :p_(NULL), length_(0), max_size_(0)
+  {
+    reserve(n);
+  }
+
+  inline ~DynArray()
+  {
+    derefer();
+  }
+  
+  inline void assign(const std::vector<VALUE_TYPE>& v)
+  {
+    if (v.size()==0)
+    {
+      clear();
+      return;
+    }
+
+    derefer();
+    
+    new_one(v.size());
+    memcpy(ARRAY(p_), v.data(), sizeof(VALUE_TYPE)*v.size());
+  }
+  
+  inline void assign(const char* p, size_t len)
+  {
+    if (!len)
+    {
+      clear();
+      return;
+    }
+
+    derefer();
+    
+    new_one(len/sizeof(VALUE_TYPE));
+    memcpy(ARRAY(p_), p, len);
+  }
+
+  inline void assign(const SelfT& other)
+  {
+    assert(this!=&other);
+    
+    if (other.length()==0)
+    {
+      clear();
+      return;
+    }
+
+    derefer();
+
+    if (COPY_ON_WRITE)
+    {
+      p_ = other.p_;
+      length_ = other.length_;
+      max_size_ = other.max_size_;
+
+      refer();
+      return;
+    }
+
+    new_one(other.length());
+    memcpy(ARRAY(p_), ARRAY(other.p_), sizeof(VALUE_TYPE)*other.length());
+  }
+
+  inline SelfT& operator = (const SelfT& other)
+  {
+    assign(other);
+    return *this;
+  }
+  
+  inline bool operator == (const SelfT& other)
+  {
+    if (length()!=other.length())
+      return false;
+    
+    size_t i=0;
+    for (; i<length(); ++i)
+      if (at(i) != other.at(i))
+        return false;
+
+    return true;
+  }
+  
+  inline bool operator < (const SelfT& other)
+  {
+    size_t i=0;
+    for (; i<length()&&i<other.length(); ++i)
+      if (at(i)<other.at(i))
+        return true;
+      else if (at(i)>other.at(i))
+        return false;
+
+    if (length()<other.length())
+      return true;
+    
+    return false;
+  }
+  
+  inline bool operator > (const SelfT& other)
+  {
+    size_t i=0;
+    for (; i<length()&&i<other.length(); ++i)
+      if (at(i)>other.at(i))
+        return true;
+      else if (at(i)<other.at(i))
+        return false;
+
+    if (length()>other.length())
+      return true;
+    
+    return false;
+  }
+
+  inline bool operator <= (const SelfT& other)
+  {
+    return *this<other || *this == other;
+  }
+
+  inline bool operator >= (const SelfT& other)
+  {
+    return *this>other || *this == other;
+  }
+  
+  inline SelfT& operator = (const std::vector<VALUE_TYPE>& other)
+  {
+    assign(other);
+    return *this;
+  }
+
+  SelfT& operator += (const SelfT& other)
+  {
+    assert(p_!= other.p_);
+    
+    if (other.length()==0)
+      return *this;
+
+    if (is_refered())
+      assign_self();
+
+    if (max_size_ < length_+other.length())
+      enlarge(length_+other.length());
+
+    memcpy(&ARRAY(p_)[length_], ARRAY(other.p_), other.size());
+    length_ += other.length();
+
+    return *this;
+  }
+
+  SelfT& operator += (const std::vector<VALUE_TYPE>& other)
+  {
+    if (other.size()==0)
+      return *this;
+
+    if (is_refered())
+      assign_self();
+
+    if (max_size_ < length_+other.size())
+      enlarge(length_+other.size());
+    
+    memcpy(&ARRAY(p_)[length_], other.data(), other.size()*sizeof(VALUE_TYPE));
+    
+    length_ += other.size();
+    
+    return *this;
+  }
+  
+  inline SelfT& operator += (VALUE_TYPE t)
+  {
+    push_back(t);
+    return *this;
+  }
+
+  inline bool operator == (const VALUE_TYPE* other)
+  {
+    for (size_t i=0; i<length_; i++)
+      if (at(i)!= other[i])
+        return false;
+
+    return true;
+  }
+  
+  inline void push_back(VALUE_TYPE t)
+  {
+    if (is_refered())
+      assign_self();
+    
+    size_t n = find_pos_2insert(t);
+    insert(n, t);    
+  }
+
+  void push_back(const SelfT& other, size_t n = 0)
+  {
+    if (other.length()==0 || n>=other.length())
+      return;
+    
+    if (is_refered())
+      assign_self();
+
+    
+    if (max_size_ < length_+other.length()-n)
+      enlarge(length_+other.length()-n);
+    
+    memcpy(&ARRAY(p_)[length_], other.data()+n, (other.length()-n)*sizeof(VALUE_TYPE));
+    
+    length_ += other.length()-n;
+  }
+  
+  inline VALUE_TYPE pop_back()
+  {
+    if (length_ ==0)
+      return VALUE_TYPE();
+    
+    if (is_refered())
+      assign_self();
+    
+    VALUE_TYPE r = at(length_-1);
+    --length_;
+    return r;
+  }
+
+  inline bool add_tail(VALUE_TYPE t)
+  {
+    // if (max_size_==0 || max_size_ == length_)
+//       enlarge((max_size_+1)<<APPEND_RATE);
+    
+    assert(length_<max_size_);
+    ARRAY(p_)[length_] = t;
+    ++length_;
+    return true;
+  }
+
+  void erase(size_t t)
+  {
+    assert(t<length_);
+    
+    if (is_refered())
+      assign_self();
+    
+    for (size_t i=t; i<length_-1; i++)
+      ARRAY(p_)[i] = ARRAY(p_)[i+1];
+    length_--;
+  }
+
+  inline void compact()
+  {
+    if (max_size_ <= length_+1)
+      return;
+    
+    if (is_refered())
+      return;
+
+    if (length_ == 0)
+    {
+      clear();
+      return;
+    }
+
+    shrink();
+  }
+
+  inline void clear()
+  {
+    derefer();
+    p_ = NULL;
+    max_size_ = length_ = 0;
+  }  
+
+  inline void insert(size_t n, VALUE_TYPE t)
+  {
+    assert(n < length_ || n == NOT_FOUND);
+    if (is_refered())
+      assign_self();
+
+    assert(max_size_>=length_);
+    
+    if (max_size_==0 || max_size_ == length_)
+      enlarge((size_t)((max_size_+1)*1.2+.5));//<<APPEND_RATE);
+    
+    for (size_t i=length_; i>n+1; i--)
+      ARRAY(p_)[i] = ARRAY(p_)[i-1];
+    
+    assert(n+1<=length_ && n+1<max_size_);
+    ARRAY(p_)[n+1] = t;
+    length_++;
+  }
+
+  size_t find(VALUE_TYPE t)const
+  {
+    if (length_ == 0)
+      return NOT_FOUND;
+    
+    if (!AUTO_SORT)
+    {
+      for (size_t i=0; i<length_; i++)
+        if (ARRAY(p_)[i]==t)
+          return i;
+
+      return NOT_FOUND;
+    }
+
+    size_t n = binary_search(t, 0, length_-1);
+    if (n == NOT_FOUND || ARRAY(p_)[n]!=t)
+      return NOT_FOUND;
+
+    return n;
+  }
+
+  inline VALUE_TYPE at (size_t t)const
+  {
+    assert(t < length_);
+    return ARRAY(p_)[t];
+  }
+
+  inline VALUE_TYPE back ()const
+  {
+    assert(length_>0);
+    return at(length_ -1);
+  }
+
+  inline VALUE_TYPE front ()const
+  {
+    assert(length_>0);
+    return at(0);
+  }
+  
+  inline VALUE_TYPE& operator [] (size_t t)
+  {
+    assert(t < length_);
+    
+     if (is_refered())
+       assign_self();
+
+    return ARRAY(p_)[t];
+  }
+
+  inline const VALUE_TYPE& operator [] (size_t t)const
+  {
+    assert(t < length_);
+    return ARRAY(p_)[t];
+  }
+
+  inline void reserve(size_t n)
+  {
+    if (n <= max_size_)
+      return;
+
+    if (is_refered())
+      assign_self();
+
+    enlarge(n);
+  }
+
+  inline void reset()
+  {
+    length_ = 0;
+  }
+
+  inline VALUE_TYPE* array(size_t n)
+  {
+    reserve(n);
+    length_ = n;
+    return ARRAY(p_);
+  }
+  
+  inline size_t length()const
+  {
+    return length_;
+  }
+
+  inline size_t capacity()const
+  {
+    return sizeof(VALUE_TYPE) * max_size_;
+  }
+
+  inline size_t size()const
+  {
+    return sizeof(VALUE_TYPE) * length_;
+  }
+
+  inline VALUE_TYPE* data()const
+  {
+    return ARRAY(p_);
+  }
+  
+
+  bool operator == (const SelfT& v)const
+  {
+    if (length_ != v.length())
+      return false;
+
+    for (size_t i=0; i<length_; i++)
+      if (ARRAY(p_)[i]!=ARRAY(v.p_)[i])
+        return false;
+    return true;
+  }
+
+  bool operator == (const std::vector<VALUE_TYPE>& v)
+  {
+    if (length_ != v.size())
+      return false;
+
+    for (size_t i=0; i<length_; i++)
+      if (ARRAY(p_)[i]!=v[i])
+        return false;
+    return true;
+  }
+
+  void sort()
+  {
+    if (length_<=1 || AUTO_SORT)
+      return;
+
+    if (is_refered())
+      assign_self();
+
+    quickSort_(ARRAY(p_), 0, length_-1);
+  }  
+
+  
+  /**
+   *This is for outputing into std::ostream, say, std::cout.
+   **/
+friend std::ostream& operator << (std::ostream& os, const SelfT& v)
+  {
+    for (size_t i =0; i<v.length_; i++)
+      ;//os<<ARRAY(v.p_)[i]<<" ";
+
+    return os;
+  }
+
+  size_t save_size()const
+  {
+    return sizeof(size_t)+size();
+  }
+  
+  size_t save(FILE* f, uint64_t addr = -1)
+  {
+    if (addr != (uint64_t)-1)
+      fseek(f, addr, SEEK_SET);
+    size_t s = size();
+    assert(fwrite(&s, sizeof(size_t), 1, f)==1);
+
+    if (s > 0)
+      assert(fwrite(ARRAY(p_), s, 1, f)==1);
+
+    return s+sizeof(size_t);
+  }
+  
+  size_t load(FILE* f, uint64_t addr = -1)
+  {
+    clear();
+    
+    if (addr != (uint64_t)-1)
+      fseek(f, addr, SEEK_SET);
+    
+    size_t s = 0;
+
+    assert(fread(&s, sizeof(size_t), 1, f)==1);
+
+    max_size_ = length_ = s/sizeof(VALUE_TYPE);
+
+    new_one(length_);
+
+    if (s > 0)
+      assert(fread(ARRAY(p_), s, 1, f)==1);
+
+    return s;
+  }
+
+  /**
+   *This is the interface for boost::serialization. Make it serializable by boost.
+   **/
+  friend class boost::serialization::access;
+  template<class Archive>
+  void save(Archive & ar, const unsigned int version)  const
+  {
+    ar & length_;
+    ar.save_binary(ARRAY(p_), size());
+  }
+
+  template<class Archive>
+  void load(Archive & ar, const unsigned int version)
+  {
+    clear();
+    //derefer();
+    
+    ar & length_;
+    max_size_ = length_;
+    new_one(length_);
+    
+    ar.load_binary(ARRAY(p_), size());
+  }
+
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
+  
+  
+}
+  ;
+
+NS_IZENELIB_AM_END
+#endif
---
+++ include/am/graph_index/id_transfer.hpp
@@ -0,0 +1,294 @@
+#ifndef ID_TRANSFER_HPP
+#define ID_TRANSFER_HPP
+
+#include<types.h>
+#include <ostream>
+#include <iostream>
+#include <assert.h>
+#include "dyn_array.hpp"
+#include "integer_hash.hpp"
+
+
+NS_IZENELIB_AM_BEGIN
+
+struct ID_64_32
+{
+  char id64[8];
+  char id32[4];
+
+  uint64_t& ID64_()
+  {
+    return *(uint64_t*)id64;
+  }
+
+  uint32_t& ID32_()
+  {
+    return *(uint32_t*)id32;
+  }
+
+  uint64_t ID64()const
+  {
+    return *(uint64_t*)id64;
+  }
+
+  uint32_t ID32()const
+  {
+    return *(uint32_t*)id32;
+  }
+  
+  inline ID_64_32(uint64_t i, uint32_t j)
+  {
+    ID64_() = i;
+    ID32_() = j;
+  }
+  
+  inline ID_64_32(uint64_t i)
+  {
+    ID64_() = i;
+    ID32_() = -1;
+  }
+  
+  inline ID_64_32()
+  {
+    ID64_() = 0;
+    ID32_() = 0;
+  }
+  
+  inline ID_64_32(const ID_64_32& other)
+  {
+    ID64_() = other.ID64();
+    ID32_() = other.ID32();
+  }
+
+  inline ID_64_32& operator = (const ID_64_32& other)
+  {
+    ID64_() = other.ID64();
+    ID32_() = other.ID32();
+    return *this;
+  }
+
+  inline bool operator == (const ID_64_32& other)const
+  {
+    return (ID64() == other.ID64());
+  }
+
+  inline bool operator != (const ID_64_32& other)const
+  {
+    return (ID64() != other.ID64());
+  }
+
+  inline bool operator < (const ID_64_32& other)const 
+  {
+    return (ID64() < other.ID64());
+  }
+
+  inline bool operator > (const ID_64_32& other)const
+  {
+    return (ID64() > other.ID64());
+  }
+
+  inline bool operator <= (const ID_64_32& other)const 
+  {
+    return (ID64() <= other.ID64());
+  }
+
+  inline bool operator >= (const ID_64_32& other)const
+  {
+    return (ID64() >= other.ID64());
+  }
+
+  inline uint32_t operator % (uint32_t e)const
+  {
+    return (ID64() % e);
+  }
+
+  
+  
+}
+  ;
+
+
+struct ID_32_64
+{
+  char id32[4];
+  char id64[8];
+
+  uint32_t& ID32_()
+  {
+    return *(uint32_t*)id32;
+  }
+
+  uint64_t& ID64_()
+  {
+    return  *(uint64_t*)id64;
+  }
+
+  uint32_t ID32()const
+  {
+    return *(uint32_t*)id32;
+  }
+
+  uint64_t ID64()const
+  {
+    return *(uint64_t*)id64;
+  }
+  
+  inline ID_32_64(uint32_t i, uint64_t j)
+  {
+    ID32_() = i;
+    ID64_() = j;
+  }
+  
+  inline ID_32_64(uint32_t i)
+  {
+    ID32_() = i;
+    ID64_() = 0;
+  }
+  
+  inline ID_32_64()
+  {
+    ID32_() = 0;
+    ID64_() = 0;
+  }
+  
+  inline ID_32_64(const ID_32_64& other)
+  {
+    ID32_() = other.ID32();
+    ID64_() = other.ID64();
+  }
+
+  inline ID_32_64& operator = (const ID_32_64& other)
+  {
+    ID32_() = other.ID32();
+    ID64_() = other.ID64();
+    return *this;
+  }
+
+  inline bool operator == (const ID_32_64& other)const
+  {
+    return (ID32() == other.ID32());
+  }
+
+  inline bool operator != (const ID_32_64& other)const
+  {
+    return (ID32() != other.ID32());
+  }
+
+  inline bool operator < (const ID_32_64& other)const 
+  {
+    return (ID32() < other.ID32());
+  }
+
+  inline bool operator > (const ID_32_64& other)const
+  {
+    return (ID32() > other.ID32());
+  }
+
+  inline bool operator <= (const ID_32_64& other)const 
+  {
+    return (ID32() <= other.ID32());
+  }
+
+  inline bool operator >= (const ID_32_64& other)const
+  {
+    return (ID32() >= other.ID32());
+  }
+
+  inline uint32_t operator % (uint32_t e)const
+  {
+    return (ID32() % e);
+  }
+
+  
+  
+}
+  ;
+
+template<
+  uint32_t START_ID = 1,
+  uint32_t ENTRY_SIZE = 100000
+  >
+class IdTransfer
+{
+  typedef IntegerHashTable<struct ID_64_32, ENTRY_SIZE> hash64_32_t;
+  typedef IntegerHashTable<struct ID_32_64, ENTRY_SIZE> hash32_64_t;
+
+  uint32_t id32_;
+  hash64_32_t table64_32_;
+  hash32_64_t table32_64_;
+  
+
+public:
+  IdTransfer()
+    :id32_(START_ID)
+  {
+  }
+
+  uint32_t insert(uint64_t id64)
+  {
+    uint32_t r = get32(id64);
+    if (r != (uint32_t)-1)
+      return r;
+
+    table64_32_.insert(ID_64_32(id64, id32_));
+
+    r = id32_;
+    assert(table32_64_.insert(ID_32_64(id32_, id64)));
+    ++id32_;
+
+    return r;
+  }
+
+  uint64_t get64(uint32_t id32)const
+  {
+    ID_32_64 id(id32);
+
+    if(!table32_64_.find(id))
+      return -1;
+    return id.ID64();
+  }
+
+  uint32_t get32(uint64_t id64)const
+  {
+    ID_64_32 id(id64);
+
+    if(!table64_32_.find(id))
+      return -1;
+    return id.ID32();
+  }
+
+  uint32_t save(FILE* f, uint64_t addr = -1)
+  {
+    if (addr != (uint64_t)-1)
+      fseek(f, addr, SEEK_SET);
+    
+    uint32_t s = sizeof(uint32_t);
+
+    assert(fwrite(&id32_, sizeof(uint32_t), 1, f)==1);
+
+    s += table32_64_.save(f);
+    s += table64_32_.save(f);
+
+    return s;
+  }
+
+  uint32_t load(FILE* f, uint64_t addr = -1)
+  {
+    if (addr != (uint64_t)-1)
+      fseek(f, addr, SEEK_SET);
+    
+    uint32_t s = sizeof(uint32_t);
+
+    assert(fread(&id32_, sizeof(uint32_t), 1, f)==1);
+
+    s += table32_64_.load(f);
+    s += table64_32_.load(f);
+
+    return s;
+  }
+  
+}
+  ;
+
+NS_IZENELIB_AM_END
+#endif
