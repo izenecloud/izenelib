744de12201a0d78884e01bfb46d50612ba1301f2
peisheng.wang <peisheng.wang@izenesoft.com>
2009-09-30 15:34:14 +0800

    add MF_CLIENT_IMPL_N_x_y macro for one to N request while MF_CLIENT_IMPL_x_y macro is for one to one request used in sf1

--- include/net/MFClientBase.h
+++ include/net/MFClientBase.h
@@ -15,16 +15,17 @@ inline void mf_client_deserialize(TYPE& result, ServiceResultPtr& res){
 		mf_deserialize(result, res);
 }
 
-/*
+
 template<typename TYPE >
 void mf_client_deserialize(std::vector<TYPE>& result, std::vector<ServiceResultPtr>& res){
 	result.resize(res.size() );
 	for(unsigned int i=0; i<res.size(); i++){
 		mf_deserialize(result[i], res[i]);
 	}	
-}*/
+}
 
-
+//for one to one
+///////////////////////////////////////////////////////
 
 #define MF_CLIENT_IMPL_0_0(agentInfo, servicename) \
 	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
@@ -236,5 +237,149 @@ void mf_client_deserialize(std::vector<TYPE>& result, std::vector<ServiceResultP
     return false; \
     
 
+//for one to N 
+///////////////////////////////////////////////////////
+#define MF_CLIENT_IMPL_N_0_0(agentInfo, servicename) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	req->setServiceName( servicename ); \
+    if ( requestService(agentInfo, servicename, req) ) \
+    { \
+	     return true; \
+	} \
+    return false; \
+
+
+#define MF_CLIENT_IMPL_N_0_1(agentInfo, servicename, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	result.resize(res.size() ); \
+	req->setServiceName( servicename ); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+    return false; \
+    
+   
+
+#define MF_CLIENT_IMPL_N_1_0(agentInfo, servicename, param1) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req); \
+    if ( requestService(agentInfo, servicename, req) ) \
+    { \
+	      return true; \
+	} \
+    return false; \
+    
+
+#define MF_CLIENT_IMPL_N_1_1(agentInfo, servicename, param1, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+    return false; \
+    
+    
+#define MF_CLIENT_IMPL_N_2_0(agentInfo, servicename, param1, param2) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+    if ( requestService(agentInfo, servicename, req) ) \
+    { \
+	      return true; \
+	} \
+    return false; \
+    
+    
+
+#define MF_CLIENT_IMPL_N_2_1(agentInfo, servicename, param1, param2, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+    	mf_client_deserialize(result, res); \
+    	return true; \
+	} \
+    return false; \
+    
+  
+    
+    
+#define MF_CLIENT_IMPL_N_3_1(agentInfo, servicename, param1, param2, param3, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+	mf_serialize(param3, req, 2); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+    return false; \
+    
+#define MF_CLIENT_IMPL_N_4_1(agentInfo, servicename, param1, param2, param3, param4, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+	mf_serialize(param3, req, 2); \
+	mf_serialize(param4, req, 3); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+    return false; \
+    
+#define MF_CLIENT_IMPL_N_5_1(agentInfo, servicename, param1, param2, param3, param4, param5, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+	mf_serialize(param3, req, 2); \
+	mf_serialize(param4, req, 3); \
+	mf_serialize(param5, req, 4); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+    return false; \
+    
+    
+#define MF_CLIENT_IMPL_N_6_1(agentInfo, servicename, param1, param2, param3, param4, param5, param6, result) \
+	ServiceRequestInfoPtr req(new ServiceRequestInfo); \
+	std::vector<ServiceResultPtr> res; \
+	req->setServiceName( servicename ); \
+	mf_serialize(param1, req, 0); \
+	mf_serialize(param2, req, 1); \
+	mf_serialize(param3, req, 2); \
+	mf_serialize(param4, req, 3); \
+	mf_serialize(param5, req, 4); \
+	mf_serialize(param6, req, 5); \
+    if ( requestService(agentInfo, servicename, req, res) ) \
+    { \
+	  	  mf_client_deserialize(result, res); \
+	      return true; \
+	} \
+	return false; \
+
 #endif
+
+
 
--- source/test/net/message-framework-testbed/process/common/IndexClient.h
+++ source/test/net/message-framework-testbed/process/common/IndexClient.h
@@ -12,14 +12,14 @@ class IndexClient:public MFClient{
 	
 public:
 	bool addDocument(const std::vector<std::string>& agentInfos, const ForwardIndex& forwardIndex){
-		MF_CLIENT_IMPL_1_0(agentInfos, "addDocument", forwardIndex)		
+		MF_CLIENT_IMPL_N_1_0(agentInfos, "addDocument", forwardIndex)		
 	}
 	
 	
 	//one agentInfo -> one docIdList(vector<unsigned int>
 	bool findDocListByTerm(const std::vector<std::string>& agentInfos,
 			std::string term, std::vector<std::vector<unsigned int> >& docIdLists){	
-		MF_CLIENT_IMPL_1_1(agentInfos, "findDocListByTerm", term, docIdLists)				
+		MF_CLIENT_IMPL_N_1_1(agentInfos, "findDocListByTerm", term, docIdLists)				
 		
 	}
 	
