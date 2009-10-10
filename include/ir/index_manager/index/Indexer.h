d69f09af18330f462e9e318253b27f88781cc866
Jia Guo <guojia@gmail.com>
2009-10-10 11:18:28 +0800

    some issue

--- include/ir/index_manager/index/Indexer.h
+++ include/ir/index_manager/index/Indexer.h
@@ -158,6 +158,11 @@ public:
     {
         return pIndexWriter_;
     }
+    
+    IndexReader* getIndexReader()
+    {
+      return pIndexReader_;
+    }
 
     BTreeIndexerInterface* getBTreeIndexer();
 
