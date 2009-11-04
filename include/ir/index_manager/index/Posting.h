1c17b08bfa7a427dfe3703c0ec3226162ccdbae0
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2009-11-04 15:50:19 +0800

    init skipping

--- include/ir/index_manager/index/Posting.h
+++ include/ir/index_manager/index/Posting.h
@@ -12,6 +12,7 @@
 #include <ir/index_manager/utility/MemCache.h>
 #include <ir/index_manager/utility/Utilities.h>
 #include <ir/index_manager/store/IndexOutput.h>
+#include <ir/index_manager/index/CompressedPostingList.h>
 
 
 NS_IZENELIB_IR_BEGIN
@@ -19,127 +20,6 @@ NS_IZENELIB_IR_BEGIN
 namespace indexmanager{
 class OutputDescriptor;
 class InputDescriptor;
-
-#pragma pack(push,1)
-struct PostingChunk
-{
-    int32_t size;
-    PostingChunk* next;
-    uint8_t data[1];
-};
-#pragma pack(pop)
-
-/// the descriptor of posting
-struct PostingDescriptor
-{
-    int64_t length; 	///length of the posting
-    count_t df; 		///document frequency of this field
-    count_t tdf;		///document frequency regarding all fields in a document
-    int64_t ctf;		///global  term frequency
-    fileoffset_t	poffset;	///offset of the position postings in the .pop file
-};
-
-/// the descriptor of chunk
-struct ChunkDescriptor
-{
-    int64_t length; 	///length of the chunk
-    docid_t lastdocid;	///the last doc id of the chunk
-};
-
-/**
-*compressed posting list
-*/
-class CompressedPostingList
-{
-public:
-    CompressedPostingList()
-            :pHeadChunk(NULL)
-            ,pTailChunk(NULL)
-            ,nTotalSize(0)
-            ,nTotalUnused(0)
-            ,nPosInCurChunk(0)
-    {
-    }
-    CompressedPostingList(const CompressedPostingList& src)
-            :pHeadChunk(src.pHeadChunk)
-            ,pTailChunk(src.pTailChunk)
-            ,nTotalSize(src.nTotalSize)
-            ,nTotalUnused(src.nTotalUnused)
-            ,nPosInCurChunk(src.nPosInCurChunk)
-    {
-    }
-    ~CompressedPostingList()
-    {
-    }
-public:
-    /**
-     * add posting which is 32 bit
-     * @param posting32 32 bit posting
-     * @return return false if no enough space, else return true.
-     */
-    bool addPosting(uint32_t posting32);
-
-    /**
-     * add posting which is 64 bit
-     * @param posting64 64 bit posting
-     * @return return false if no enough space, else return true.
-     */
-    bool addPosting(uint64_t posting64);
-
-    /**
-     * decode 32bit posting
-     * @param posting the posting
-     * @return decoded value
-     */
-    static int32_t decodePosting32(uint8_t*& posting);
-
-    /**
-     * decode 64bit posting
-     * @param posting the posting
-     * @return decoded value
-     */
-    static int64_t decodePosting64(uint8_t*& posting);
-
-    /**
-     * encode 32bit posting
-     * @param posting the encoded posting
-     * @param val value needed to encode
-     */
-    static void encodePosting32(uint8_t*& posting,int32_t val);
-
-    /**
-     * encode 64bit posting
-     * @param posting the posting
-     * @param val value needed to encode
-     */
-    static void encodePosting64(uint8_t*& posting,int64_t val);
-
-    /**
-     * add a new node to the list
-     * @param pNode the node
-     */
-    void addChunk(PostingChunk* pChunk);
-
-    /** truncation the tail chunk,let chunk size=real used size of this chunk */
-    void truncTailChunk();
-
-    /** get the real size of the list */
-    int32_t getRealSize();
-
-    /**
-     * reset the list for using at next time
-     */
-    void reset();
-protected:
-    PostingChunk* pHeadChunk;		///Posting list header
-    PostingChunk* pTailChunk;			///Posting list tail
-    uint32_t nTotalSize;			///Total size
-    uint32_t nTotalUnused;		///Total Unused size
-    uint32_t nPosInCurChunk;
-
-    friend class InMemoryPosting;
-    friend class PostingMerger;
-};
 
 /**
 *virtual base class of InMemoryPosting and OnDiskPosting
