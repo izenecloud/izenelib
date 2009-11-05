de575648e8defea797ea3a3370282c938c19d9aa
Ian Yang <doit.ian@gmail.com>
2009-11-05 13:29:48 +0800

    derive index manager exception from std::runtime_error and make it more safe

--- include/ir/index_manager/utility/Exception.h
+++ include/ir/index_manager/utility/Exception.h
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdexcept>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/lexical_cast.hpp>
 
 NS_IZENELIB_IR_BEGIN
 
@@ -39,91 +43,154 @@ enum ExceptionCode
     NUM_ERRORS
 };
 
-///IndexManagerException
-class IndexManagerException
+class IndexManagerException : public std::runtime_error
 {
 public:
-    IndexManagerException(exception_type code,const std::string& msg = ""):_what(s_errorStrings[code] + ":" + msg),_code(code) {}
-    IndexManagerException(exception_type code,const std::string& msg,const std::string& file,int line) : _code(code)
+    IndexManagerException(exception_type code)
+    : std::runtime_error("")
+    , code_(code)
     {
-        std::stringstream ss;
-        ss << s_errorStrings[code] << ":" << msg << " at file: " << file << ",line:" << line;
-        _what = ss.str();
-        std::cout << _what << std::endl;
+        try
+        {
+            imp_.reset(new Imp_);
+        }
+        catch (...)
+        {
+            imp_.reset();
+        }
     }
-    IndexManagerException(const IndexManagerException& clone):_what(clone._what),_code(clone._code)
+    IndexManagerException(exception_type code, const std::string& msg)
+    : std::runtime_error(msg)
+    , code_(code)
     {
+        try
+        {
+            imp_.reset(new Imp_);
+        }
+        catch (...)
+        {
+            imp_.reset();
+        }
     }
-    IndexManagerException&operator=(const IndexManagerException& right)
+    IndexManagerException(exception_type code,
+                          const std::string& msg,
+                          const std::string& file,
+                          int line)
+    : std::runtime_error(msg)
+    , code_(code)
     {
-        _code = right._code;
-        _what = right._what;
-        return *this;
-    }
-    virtual ~IndexManagerException() {}
-    virtual const char* what()
-    {
-        return _what.c_str();
-    }
-    virtual exception_type	code()
-    {
-        return _code;
+        try
+        {
+            imp_.reset(new Imp_);
+            imp_->file = file;
+            imp_->line = line;
+        }
+        catch (...)
+        {
+            imp_.reset();
+        }
     }
 
-protected:
-    static std::string 		s_errorStrings[NUM_ERRORS];
-    std::string			_what;
-    exception_type	_code;
+    const char* what() const throw()
+    // see http://www.boost.org/more/error_handling.html for lazy build rationale
+    {
+        if (imp_.get())
+        {
+            try
+            {
+                if (imp_->what.empty())
+                {
+                    imp_->what = std::runtime_error::what();
+                    if (!imp_->what.empty())
+                    {
+                        imp_->what += ": ";
+                    }
+                    if (code_ < NUM_ERRORS)
+                    {
+                        imp_->what = s_errorStrings[code_];
+                    }
+                    else
+                    {
+                        imp_->what = s_errorStrings[ERROR_UNKNOWN];
+                    }
+                    if (!imp_->file.empty())
+                    {
+                        imp_->what += " at ";
+                        imp_->what += imp_->file;
+                        imp_->what += "(";
+                        imp_->what +=
+                            boost::lexical_cast<std::string>(imp_->line);
+                        imp_->what += ")";
+                    }
+                }
+                return imp_->what.c_str();
+            }
+            catch (...)
+            {
+                imp_->what.clear();
+            }
+        }
+
+        return std::runtime_error::what();
+    }
+    virtual exception_type code() const throw()
+    {
+        return code_;
+    }
+
+private:
+    exception_type code_;
+    struct Imp_
+    {
+        std::string file;
+        int line;
+        std::string what;
+    };
+    boost::shared_ptr<Imp_> imp_;
+
+    static std::string s_errorStrings[NUM_ERRORS];
 };
-///IllegalArgumentException
+
 class IllegalArgumentException : public IndexManagerException
 {
 public:
-    IllegalArgumentException(const std::string& msg):IndexManagerException(ERROR_ILLEGALARGUMENT,msg)
-    {
-    }
-    ~IllegalArgumentException() {}
+    IllegalArgumentException(const std::string& msg)
+    : IndexManagerException(ERROR_ILLEGALARGUMENT, msg)
+    {}
 };
-///UnsupportedOperationException
 class UnsupportedOperationException : public IndexManagerException
 {
 public:
-    UnsupportedOperationException(const std::string& msg):IndexManagerException(ERROR_UNSUPPORTED,msg)
-    {
-    }
-    ~UnsupportedOperationException() {}
+    UnsupportedOperationException(const std::string& msg)
+    : IndexManagerException(ERROR_UNSUPPORTED, msg)
+    {}
 };
-///FileIOException
 class FileIOException : public IndexManagerException
 {
 public:
-    FileIOException(const std::string& msg):IndexManagerException(ERROR_FILEIO,msg)
-    {
-    }
-    ~FileIOException() {}
+    FileIOException(const std::string& msg)
+    : IndexManagerException(ERROR_FILEIO, msg)
+    {}
 };
-
-///OutOfMemoryException
 class OutOfMemoryException : public IndexManagerException
 {
 public:
-    OutOfMemoryException (const std::string& msg) : IndexManagerException(ERROR_OUTOFMEM,msg) {}
-    ~OutOfMemoryException() {}
+    OutOfMemoryException(const std::string& msg)
+    : IndexManagerException(ERROR_OUTOFMEM, msg)
+    {}
 };
-///NetworkException
 class NetworkException : public IndexManagerException
 {
 public:
-    NetworkException (const std::string& msg) : IndexManagerException(ERROR_NETWORK,msg) {}
-    ~NetworkException() {}
+    NetworkException(const std::string& msg)
+    : IndexManagerException(ERROR_NETWORK, msg)
+    {}
 };
 
-#define SF1V5_THROW(code,msg) throw izenelib::ir::indexmanager::IndexManagerException(code,msg,__FILE__, __LINE__)
-#define SF1V5_RETHROW(e) throw izenelib::ir::indexmanager::IndexManagerException(e)
-
+#define SF1V5_THROW(code,msg) throw ::izenelib::ir::indexmanager::IndexManagerException(code, msg, __FILE__, __LINE__)
+#define SF1V5_RETHROW(e) throw ::izenelib::ir::indexmanager::IndexManagerException(e)
 
 }
-
 
 NS_IZENELIB_IR_END
 
