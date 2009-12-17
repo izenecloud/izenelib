dd09c6f7c3b06eca7157ee18e2a91882000b7596
wei.cao <wei.cao@izenesoft.com>
2009-12-17 18:00:14 +0800

    add test

---
+++ source/test/hdb/t_bench.cpp
@@ -0,0 +1,136 @@
+/// @file   t_sdb.cpp
+/// @brief  A test unit for checking class ScalableDB's correctness.
+/// @author Wei Cao
+/// @date   2000-08-14
+///
+///
+/// @details
+///
+
+
+#include <string>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <wiselib/ustring/UString.h>
+#include <hdb/HugeDB.h>
+#include <util/ProcMemInfo.h>
+
+using namespace std;
+using namespace wiselib;
+using namespace izenelib::am;
+using namespace izenelib::hdb;
+
+// pseudo random number generator
+inline int myrand(void) {
+	static int cnt = 0;
+	return (lrand48() + cnt++) & 0x7FFFFFFF;
+}
+
+void displayMemInfo(std::ostream& os = std::cout) {
+	unsigned long rlimit = 0, vm = 0, rss = 0;
+	ProcMemInfo::getProcMemInfo(vm, rss, rlimit);
+	os << "vm: " << vm << "bytes rss: " << rss << "bytes" << endl;
+}
+
+BOOST_AUTO_TEST_SUITE( sdb_bench_suite )
+
+BOOST_AUTO_TEST_CASE(bench_insert)
+{
+    displayMemInfo();
+
+    {
+    std::cout << "building hdb" << std::endl;
+    //ordered_hdb_no_delta< wiselib::UString, wiselib::UString> hdb("bench0");
+    ordered_hdb_no_delta< std::string, std::string> hdb("bench0");
+
+    //ordered_hdb_no_delta< int, int> hdb("bench0");
+    hdb.setMergeFactor(2);
+    hdb.setCachedRecordsNumber(100000);
+    hdb.open();
+
+    boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
+    for(int i =0; i<1000000; i++ )
+    {
+        if(i%100000 == 0) {
+            std::cout << "insert " << i << " elements" << std::endl;
+            displayMemInfo();
+        }
+	std::string k = boost::lexical_cast<std::string>(myrand() );
+	std::string v = boost::lexical_cast<std::string>(myrand() );
+	wiselib::UString key = k;
+	wiselib::UString value = v;
+        //hdb.insertValue(key,value);
+        hdb.insertValue(k,v);
+        //hdb.insertValue(myrand(),myrand());
+    }
+    hdb.close();
+    boost::posix_time::time_duration td = boost::posix_time::microsec_clock::local_time() - start;
+    std::cout << "build hdb cost " << td.total_microseconds()/1000000 << " seconds" << std::endl;
+    }
+
+    displayMemInfo();
+}
+
+//
+//BOOST_AUTO_TEST_CASE(bench_find)
+//{
+//    std::cout << "find in unoptimized hdb" << std::endl;
+//    ordered_hdb_fixed<int, int> hdb("bench0");
+//    hdb.setMergeFactor(4);
+//    hdb.setCachedRecordsNumber(5000000);
+//    hdb.open();
+//
+//    boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
+//    for(int i =0; i<10000; i++ )
+//    {
+//        if(i%1000 == 0) {
+//            std::cout << "find " << i << " elements" << std::endl;
+//            displayMemInfo();
+//        }
+//        hdb.getValue(myrand(), i);
+//    }
+//
+//    hdb.close();
+//    boost::posix_time::time_duration td = boost::posix_time::microsec_clock::local_time() - start;
+//    std::cout << "find in hdb cost " << td.total_microseconds()/1000000 << " seconds";
+//}
+
+//
+//BOOST_AUTO_TEST_CASE(bench_find_after_optimized)
+//{
+//    std::cout << "find in unoptimized hdb" << std::endl;
+//    ordered_hdb_fixed<int, int> hdb("bench0");
+//    hdb.setMergeFactor(4);
+//    hdb.setCachedRecordsNumber(5000000);
+//    hdb.open();
+//
+//    hdb.optimize();
+//    std::cout << hdb.numItems() << std::endl;
+//
+//    boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
+//    for(int i =0; i<100000; i++ )
+//    {
+//        if(i%10000 == 0) {
+//            std::cout << "find " << i << " elements" << std::endl;
+//            displayMemInfo();
+//        }
+//        hdb.getValue(myrand(), i);
+//    }
+//    for(int i =0; i<1000; i++ )
+//    {
+//        if(i%100 == 0) {
+//            std::cout << "find " << i << " elements" << std::endl;
+//            displayMemInfo();
+//        }
+//        hdb.getValue(myrand(), i);
+//    }
+//
+//    hdb.close();
+//    boost::posix_time::time_duration td = boost::posix_time::microsec_clock::local_time() - start_;
+//    std::cout << "find in hdb cost " << td.micro_seconds()/1000000 << " seconds";
+//}
+
+
+BOOST_AUTO_TEST_SUITE_END()
