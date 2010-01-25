ae6ec09cf6a0a0c73cf7d41686f427f5ff1205ee
Kevin Hu <kevinhu.sh@gmail.com>
2010-01-25 09:09:53 +0800

    add mr wordID

---
+++ include/ir/mr_word_id/mister_wordID.hpp
@@ -0,0 +1,471 @@
+/**
+   @file mister_wordID.h
+   @auther Kevin Hu
+   @date 2010.01.09
+ **/
+
+#ifndef MISTER_WORDID_HPP
+#define MISTER_WORDID_HPP
+
+#include "term_freq.hpp"
+#include "id_str_table.hpp"
+#include <string>
+#include <vector>
+#include <wiselib/ustring/UString.h>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/function.hpp>
+#include <am/graph_index/dyn_array.hpp>
+#include <cmath>
+
+NS_IZENELIB_IR_BEGIN
+
+template<
+  wiselib::UString::EncodingType ENCODE_TYPE = wiselib::UString::UTF_8,
+  uint32_t START_ID = 1
+  >
+class MisterWordID
+{
+  typedef IdStringTable<uint32_t> id_table_t;
+  typedef TermFrequency<> term_freq_t;
+  typedef TermFrequency<> str_table_t;
+  typedef boost::function< std::vector<std::string> ( const std::string&) > participle_t;
+  typedef izenelib::am::DynArray<double> log_weights_t;
+  
+  term_freq_t term_freq_;
+  id_table_t id_table_;
+  str_table_t str_table_;
+  std::string filenm_;
+  term_freq_t stop_word_dic_;
+  log_weights_t log_weights_;
+  
+  //participle_t participle_;
+
+  bool init_stop_word_(const char* dicname)
+  {
+    FILE* f = fopen(dicname, "r");
+    if (f == NULL)
+      return false;
+
+    fseek(f, 0, SEEK_END);
+    uint64_t fs = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    if (fs == 0)
+      return false;
+    
+    uint64_t pos_s = 0;
+    uint64_t pos_e = 0;
+    
+    char* buff = new char[fs];
+    IASSERT(fread(buff, fs, 1, f)==1);
+    fclose(f);
+
+    while (pos_s < fs && pos_e<=fs)
+    {
+      if (buff[pos_e]!='\n')
+      {
+        ++pos_e;
+        continue;
+      }
+
+      if (pos_e - pos_s <=1)
+      {
+        ++pos_e;
+        pos_s = pos_e;
+        continue;
+      }
+      
+      std::string line(&buff[pos_s], pos_e - pos_s);
+
+      stop_word_dic_.increase(line);
+
+      ++pos_e;
+      pos_s = pos_e;
+    }
+
+    delete buff;
+    return true;
+  }
+
+  bool init_term_freq_(const char* dicname)
+  {
+    FILE* f = fopen(dicname, "r");
+    if (f == NULL)
+    {
+      std::cout<<dicname<<" is not exist!\n";
+      return false;
+    }
+
+    fseek(f, 0, SEEK_END);
+    uint64_t fs = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    if (fs == 0)
+    {
+      std::cout<<dicname<<" is not supposed to be empty!\n";
+      return false;
+    }
+    
+    uint64_t pos_s = 0;
+    uint64_t pos_e = 0;
+    
+    char* buff = new char[fs];
+    IASSERT(fread(buff, fs, 1, f)==1);
+    fclose(f);
+
+    while (pos_s < fs && pos_e<=fs)
+    {
+      if (buff[pos_e]!='\n')
+      {
+        ++pos_e;
+        continue;
+      }
+
+      if (pos_e - pos_s <=1)
+      {
+        ++pos_e;
+        pos_s = pos_e;
+        continue;
+      }
+      
+      std::string line(&buff[pos_s], pos_e - pos_s);
+      if (stop_word_dic_.find(line)==0)
+        term_freq_.increase(line);
+
+      ++pos_e;
+      pos_s = pos_e;
+    }
+
+    delete buff;
+    return true;
+  }
+
+  bool traversal_corpus_(const char* corpus_dir, participle_t participle)
+  {
+    namespace fs = boost::filesystem;
+    const static uint32_t bs = 1000000;
+    
+    fs::path full_path(corpus_dir, fs::native);
+    if (!fs::exists(full_path))
+    {
+      std::cout<<corpus_dir<<" is not exist!\n";
+      return false;
+    }
+
+    fs::directory_iterator item_b(full_path);
+    fs::directory_iterator item_e;
+    char* buff = new char[bs];
+    for (; item_b!=item_e; item_b++)
+    {
+      if (fs::is_directory(*item_b))
+        continue;
+
+      std::string nm = item_b->path().file_string();
+      FILE* f = fopen(nm.c_str(), "r");
+      IASSERT(f!=NULL);
+      
+      fseek(f, 0, SEEK_END);
+      uint64_t fs = ftell(f);
+      fseek(f, 0, SEEK_SET);
+      const uint64_t FS = fs;
+
+      std::cout<<std::endl;
+      while (fs > 0)
+      {
+        std::cout<<"\rScanning ... "<<(1.-(fs*1./FS))*100.0<<"%"<<std::flush;
+        uint32_t rs = bs > fs? fs: bs;
+        IASSERT(fread(buff, rs, 1, f)==1);
+        fs -= rs;
+
+        //std::cout<<fs<<" "<<rs<<std::endl;
+        uint32_t pos_s = 0;
+        uint32_t pos_e = 0;
+        std::vector<std::string> terms;
+        while (pos_s < rs && pos_e<=rs)
+        {
+          if (!(fs ==0 && pos_e==rs)&& buff[pos_e]!='\n')
+          {
+            ++pos_e;
+            continue;
+          }
+
+          if (pos_e - pos_s <=1)
+          {
+            ++pos_e;
+            pos_s = pos_e;
+            continue;
+          }
+      
+          std::string line(&buff[pos_s], pos_e - pos_s);
+          terms = participle(line);
+
+          for (uint32_t i=0; i<terms.size(); ++i)
+            if (term_freq_.find(terms[i]))
+              term_freq_.increase(terms[i]);
+
+          ++pos_e;
+          pos_s = pos_e;
+        }
+        
+        fseek(f, -1*(int)(pos_e-pos_s), SEEK_CUR);
+        fs += pos_e - pos_s;
+      }
+
+      fclose(f);
+    }
+
+    delete buff;
+
+    return true;
+  }
+
+  struct FREQ_STRUCT
+  {
+    uint32_t freq;
+    uint32_t idx;
+
+    uint32_t& FREQ_()
+    {
+      return freq;
+    }
+
+    uint32_t& IDX_()
+    {
+      return idx;
+    }
+
+    uint32_t FREQ()const
+    {
+      return freq;
+    }
+
+    uint32_t IDX()const
+    {
+      return idx;
+    }
+  
+    inline FREQ_STRUCT(uint32_t i, uint32_t j)
+    {
+      FREQ_() = i;
+      IDX_() = j;
+    }
+  
+    inline FREQ_STRUCT(uint32_t i)
+    {
+      FREQ_() = i;
+      IDX_() = 0;
+    }
+  
+    inline FREQ_STRUCT()
+    {
+      FREQ_() = 0;
+      IDX_() = -1;
+    }
+  
+    inline FREQ_STRUCT(const FREQ_STRUCT& other)
+    {
+      FREQ_() = other.FREQ();
+      IDX_() = other.IDX();
+    }
+
+    inline FREQ_STRUCT& operator = (const FREQ_STRUCT& other)
+    {
+      FREQ_() = other.FREQ();
+      IDX_() = other.IDX();
+      return *this;
+    }
+
+    inline bool operator == (const FREQ_STRUCT& other)const
+    {
+      return (FREQ() == other.FREQ());
+    }
+
+    inline bool operator != (const FREQ_STRUCT& other)const
+    {
+      return (FREQ() != other.FREQ());
+    }
+
+    inline bool operator < (const FREQ_STRUCT& other)const 
+    {
+      return (FREQ() < other.FREQ());
+    }
+
+    inline bool operator > (const FREQ_STRUCT& other)const
+    {
+      return (FREQ() > other.FREQ());
+    }
+
+    inline bool operator <= (const FREQ_STRUCT& other)const 
+    {
+      return (FREQ() <= other.FREQ());
+    }
+
+    inline bool operator >= (const FREQ_STRUCT& other)const
+    {
+      return (FREQ() >= other.FREQ());
+    }
+
+    inline uint32_t operator & (uint32_t e)const
+    {
+      return (FREQ() & e);
+    }
+
+    /**
+     *This is for outputing into std::ostream, say, std::cout.
+     **/
+  friend std::ostream& operator << (std::ostream& os, const FREQ_STRUCT& v)
+    {
+      os<<"<"<<v.FREQ()<<","<<v.IDX()<<">";
+
+      return os;
+    }
+  
+  }
+    ;
+
+  void sort_freq_()
+  {
+    typedef izenelib::am::DynArray<FREQ_STRUCT> freq_t;
+
+    if (!term_freq_.begin())
+      return;
+
+    freq_t freqs;
+    std::vector<std::string> words;
+    
+    std::string word;
+    uint32_t freq = 0;
+    while(term_freq_.next(word, freq))
+    {
+      freqs.push_back(FREQ_STRUCT(freq, words.size()));
+      words.push_back(word);
+    }
+
+    freqs.sort();
+
+    for (uint32_t i= freqs.length()-1; i!=(uint32_t)-1; --i)
+    {
+      str_table_.insert(words[freqs.at(i).IDX()], freqs.length()-1-i+START_ID);
+      id_table_.insert(freqs.length()-1-i+START_ID, words[freqs.at(i).IDX()].length(),
+                       words[freqs.at(i).IDX()].c_str());
+    }
+
+    FILE* f = fopen(std::string(filenm_+".dic").c_str(), "w+");
+    term_freq_.save(f);
+    fclose(f);
+
+    f = fopen(std::string(filenm_+".stw").c_str(), "w+");
+    stop_word_dic_.save(f);
+    fclose(f);
+
+    f = fopen(std::string(filenm_+".str.id").c_str(), "w+");
+    str_table_.save(f);
+    fclose(f);
+
+    f = fopen(std::string(filenm_+".id.str").c_str(), "w+");
+    id_table_.save(f);
+    fclose(f);
+
+    f = fopen(std::string(filenm_+".over").c_str(), "w+");
+    fclose(f);
+  }
+
+  bool load_()
+  {
+    FILE* f = fopen(std::string(filenm_+".over").c_str(), "r");
+    if (f == NULL)
+      return false;
+    fclose(f);
+    
+    f = fopen(std::string(filenm_+".stw").c_str(), "r");
+    IASSERT(f!=NULL);    
+    stop_word_dic_.load(f);
+    fclose(f);
+    
+    f = fopen(std::string(filenm_+".str.id").c_str(), "r");
+    IASSERT(f!=NULL);    
+    str_table_.load(f);
+    fclose(f);
+
+    f = fopen(std::string(filenm_+".id.str").c_str(), "r");
+    IASSERT(f!=NULL);
+    id_table_.load(f);
+    fclose(f);
+
+    return true;
+  }
+
+  inline void prepare_weights_()
+  {
+    log_weights_.reserve(num_items());
+    for (uint32_t i=0; i<num_items(); ++i)
+    {
+      log_weights_.push_back(log10(num_items()-i+2));
+    }
+  }
+  
+ public:
+  MisterWordID(const char* filenm)
+    :filenm_(filenm)
+  {
+  }
+
+  ~MisterWordID()
+  {
+  }
+
+  bool is_ready()
+  {
+    if(load_())
+    {
+      prepare_weights_();
+      return true;
+    }
+    return false;
+  }
+  
+  bool prepareID(const char* dicname, const char* corpus_dir, participle_t participle, const char* sw_dic="")
+  {
+    init_stop_word_(sw_dic);
+    
+    if (!init_term_freq_(dicname))
+      return false;
+    if (!traversal_corpus_(corpus_dir, participle))
+      return false;
+
+    sort_freq_();
+
+    prepare_weights_();
+    return true;
+  }
+
+  uint32_t get_id(const std::string& word )const
+  {
+    return str_table_.find(word);
+  }
+
+  inline double get_weight(uint32_t id)const
+  {
+    if (id>=log_weights_.length())
+      id = log_weights_.length()-1;
+    
+    return log_weights_.at(id);
+  }
+
+  std::string get_word(uint32_t id)const
+  {
+    uint32_t len = 0;
+    const char* p = id_table_.find(id, len);
+    return std::string(p, len);
+  }
+
+  uint32_t num_items()const
+  {
+    return id_table_.num_items();
+  }
+  
+}
+;
+
+NS_IZENELIB_IR_END
+
+#endif//MISTER_WORDID_H
---
+++ include/ir/mr_word_id/term_freq.hpp
@@ -0,0 +1,401 @@
+/**
+   @file ter_freq.hpp
+   @author Kevin Hu
+   @date 2010.01.08
+*/
+#ifndef TERM_FREQ_HPP
+#define TERM_FREQ_HPP
+
+#include <types.h>
+
+#include <cstdlib>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <util/hashFunction.h>
+
+NS_IZENELIB_IR_BEGIN
+/**
+ * @class TermFrequency
+ * @brief TermFrequency is used for caculating the frequency of terms. It's a string hash table essentially.
+ *
+ **/
+
+template<
+  typename LenType = uint8_t,
+  uint8_t ENTRY_POW = 17
+  >
+class TermFrequency
+{
+  enum {ENTRY_SIZE = (2<<ENTRY_POW)};
+  enum {ENTRY_MASK = ENTRY_SIZE-1};
+  enum 
+    {
+      INIT_BUCKET_SIZE = 64
+    };
+    
+
+protected:
+  char** entry_;//!< entry of table
+  uint32_t count_;
+  uint32_t bkt_iter_;
+  uint32_t iter_;
+
+  void clean_()
+  {
+    for(uint32_t i=0; i<ENTRY_SIZE; i++)
+    {
+      if (entry_[i]!= NULL)
+        free (entry_[i]);
+      
+      entry_[i] = NULL;
+    }
+
+    count_ = 0;
+  }
+  
+public:
+  /**
+     @brief a constructor
+  */
+  TermFrequency()
+  {
+    entry_ = new char*[ENTRY_SIZE];
+    for(size_t i=0; i<ENTRY_SIZE; i++)
+    {
+      entry_[i] = NULL;
+    }
+    count_ = 0;
+  }
+    
+  /**
+     @brief a destructor
+  */
+  ~TermFrequency()
+  {
+    clean_();
+    delete entry_;
+  }
+
+  uint32_t num_items() const
+  {
+    return count_;
+  }
+
+  void display(std::ostream & os=std::cout)
+  {
+    for(size_t i=0; i<ENTRY_SIZE; i++)
+    {
+      char* pBkt = entry_[i];
+
+      if (pBkt ==NULL)
+        continue;
+
+      os<<endl<<i<<" Entry===========\n";
+
+      uint32_t bs = *(uint32_t*)(pBkt);
+      os<<"bucket size: "<<bs<<endl;
+      os<<"content size: "<<*((uint32_t*)(pBkt)+1)<<endl;
+
+      uint32_t p = sizeof(uint32_t)*2;
+      uint32_t c = *((uint32_t*)(pBkt)+1);
+            
+      while (p < c)
+      {
+        LenType len = *((LenType*)(pBkt+p));
+        p += sizeof (LenType);
+        os<<"("<<(uint32_t)len<<")";
+
+        os<<(pBkt+p);
+        p += len;
+
+        os<<"=>"<<*((uint32_t*)(pBkt+p))<<std::endl;
+        p += sizeof (uint32_t);
+      }
+    }
+  
+  }
+
+  bool increase(const std::string& term)
+  {
+    if (term.length()==0)
+      return false;
+
+    uint32_t content_len = 0;
+    uint32_t bs = 0;
+    
+    uint32_t idx = izenelib::util::HashFunction<std::string>::generateHash32(term) & ENTRY_MASK;
+
+    char* pBkt = entry_[idx];
+
+    if (pBkt == NULL)
+    {
+      entry_[idx] = pBkt = (char*)malloc(INIT_BUCKET_SIZE);
+      bs = *(uint32_t*)(pBkt) = INIT_BUCKET_SIZE;
+      content_len = *((uint32_t*)(pBkt)+1) = 2*sizeof(uint32_t);
+    }
+    else
+    {//does it exist?
+      bs = *(uint32_t*)(pBkt);
+      content_len = *((uint32_t*)pBkt+1);
+      uint32_t p = sizeof(uint32_t)*2;;
+      while (p < content_len)
+      {
+        LenType len = *((LenType*)(pBkt+p));
+        p += sizeof (LenType);
+
+        if (len-1 != (LenType)term.length())
+        {
+          p += len + sizeof (uint32_t);
+          continue;
+        }
+
+        LenType j=0;
+        for (; j<len-1; j++)
+          if (term[j] != pBkt[p+j])
+            break;
+
+        if (j++ == len-1)
+        {
+          ++(*(uint32_t*)(pBkt+p+j));
+          return true;
+        }
+
+        p += len + sizeof (uint32_t);
+      }
+    }
+
+    IASSERT(bs>=content_len);
+    
+    while(bs-content_len<term.length()+1+sizeof(LenType)+sizeof(uint32_t))
+    {
+      bs += INIT_BUCKET_SIZE;
+
+      if (bs-content_len<term.length()+1+sizeof(LenType)+sizeof(uint32_t))
+        continue;
+      //content_len += str.length()+sizeof(uint32_t);
+      pBkt = (char*)realloc(pBkt, bs);
+      entry_[idx] = pBkt;
+    }
+
+    if (term.length()+1>=(LenType)-1)
+      std::cout<<"[ERROR] in TermFrequency increase(): Term length too long! =>"<<term<<std::endl;
+    
+    *((LenType*)(pBkt+content_len)) = term.length()+1;
+    content_len += sizeof(LenType);
+    memcpy(pBkt+content_len, term.c_str(), term.length());
+    content_len += term.length();
+    *(pBkt+content_len) = '\0';
+    ++content_len; 
+        
+    *((uint32_t*)(pBkt+content_len)) = 1;
+    content_len += sizeof(uint32_t);
+
+    *(uint32_t*)(pBkt) = bs;
+    *((uint32_t*)pBkt+1) = content_len;
+
+    count_++;
+    return false;
+  }
+
+  bool insert(const std::string& term, uint32_t freq)
+  {
+    if (term.length()==0)
+      return false;
+
+    if (find(term)>0)
+      return false;
+    
+    uint32_t content_len = 0;
+    uint32_t bs = 0;
+    
+    uint32_t idx = izenelib::util::HashFunction<std::string>::generateHash32(term) & ENTRY_MASK;
+
+    char* pBkt = entry_[idx];
+
+    if (pBkt == NULL)
+    {
+      entry_[idx] = pBkt = (char*)malloc(INIT_BUCKET_SIZE);
+      bs = *(uint32_t*)(pBkt) = INIT_BUCKET_SIZE;
+      content_len = *((uint32_t*)(pBkt)+1) = 2*sizeof(uint32_t);
+    }
+    else
+    {//does it exist?
+      bs = *(uint32_t*)(pBkt);
+      content_len = *((uint32_t*)pBkt+1);
+    }
+    
+    IASSERT(bs>=content_len);
+    
+    while(bs-content_len<term.length()+1+sizeof(LenType)+sizeof(uint32_t))
+    {
+      bs += INIT_BUCKET_SIZE;
+
+      if (bs-content_len<term.length()+1+sizeof(LenType)+sizeof(uint32_t))
+        continue;
+      //content_len += str.length()+sizeof(uint32_t);
+      pBkt = (char*)realloc(pBkt, bs);
+      entry_[idx] = pBkt;
+    }
+
+    if (term.length()+1>=(LenType)-1)
+      std::cout<<"[ERROR] in TermFrequency insert(): Term length too long! =>"<<term<<std::endl;
+    
+    *((LenType*)(pBkt+content_len)) = term.length()+1;
+    content_len += sizeof(LenType);
+    memcpy(pBkt+content_len, term.c_str(), term.length());
+    content_len += term.length();
+    *(pBkt+content_len) = '\0';
+    ++content_len; 
+        
+    *((uint32_t*)(pBkt+content_len)) = freq;
+    content_len += sizeof(uint32_t);
+
+    *(uint32_t*)(pBkt) = bs;
+    *((uint32_t*)pBkt+1) = content_len;
+
+    count_++;
+    return false;
+  }
+
+  uint32_t find(const std::string& term)const
+  {
+    if (term.length()==0)
+      return 0;
+    
+    uint32_t idx = izenelib::util::HashFunction<std::string>::generateHash32(term) & ENTRY_MASK;
+
+    char* pBkt = entry_[idx];
+
+    if (pBkt == NULL)
+      return 0;
+    
+    uint32_t  bs = *(uint32_t*)(pBkt);
+    uint32_t  content_len = *((uint32_t*)pBkt+1);
+
+    IASSERT(bs>=content_len);
+    uint32_t p = sizeof(uint32_t)*2;;
+
+    while (p < content_len)
+    {
+      LenType len = *((LenType*)(pBkt+p));
+      p += sizeof (LenType);
+
+      if (len-1 != (LenType)term.length())
+      {
+        p += len + sizeof (uint32_t);
+        continue;
+      }
+
+      LenType j=0;
+      for (; j<len-1; j++)
+        if (term[j] != pBkt[p+j])
+          break;
+
+      if (j++ == len-1)
+        return (*(uint32_t*)(pBkt+p+j));
+
+      p += (uint32_t)len + sizeof (uint32_t);
+    }
+
+    return 0;
+  }
+
+  void save(FILE* f)const
+  {
+    char* pBkt = NULL;
+    fseek(f, 0, SEEK_SET);
+    IASSERT(fwrite(&count_, sizeof(uint32_t), 1, f)==1);
+    
+    uint32_t i=0;
+    for(; i<ENTRY_SIZE; i++)
+    {
+      if (entry_[i]==NULL)
+        continue;
+
+      IASSERT(fwrite(&i, sizeof(uint32_t), 1, f)==1);
+      pBkt = entry_[i];
+      *(uint32_t*)(pBkt) = *((uint32_t*)pBkt+1);
+      IASSERT(fwrite(pBkt, *(uint32_t*)(pBkt), 1, f)==1);
+    }
+
+    i=-1;
+
+    IASSERT(fwrite(&i, sizeof(uint32_t), 1, f)==1);
+  }
+  
+  void load(FILE* f)
+  {
+    clean_();
+    
+    char* pBkt = NULL;
+    uint32_t i=0;
+    uint32_t bs = 0;
+    
+    fseek(f, 0, SEEK_SET);
+    IASSERT(fread(&count_, sizeof(uint32_t), 1, f)==1);
+
+    IASSERT(fread(&i, sizeof(uint32_t), 1, f)==1);
+    while (i != (uint32_t)-1)
+    {
+      IASSERT(fread(&bs, sizeof(uint32_t), 1, f)==1);
+      pBkt = entry_[i] = (char*)malloc(bs);
+      *(uint32_t*)(pBkt) = bs;
+      IASSERT(fread(pBkt+sizeof(uint32_t), bs-sizeof(uint32_t), 1, f)==1);
+      
+      IASSERT(fread(&i, sizeof(uint32_t), 1, f)==1);
+    }
+  }
+
+  bool begin()
+  {
+    for(uint32_t i=0; i<ENTRY_SIZE; i++)
+      if (entry_[i]!= NULL)
+      {
+        bkt_iter_ = i;
+        iter_ = sizeof(uint32_t)*2;
+        return true;
+      }
+
+    bkt_iter_ = -1;
+    iter_ = -1;
+    return false;
+  }
+
+  bool next(std::string& term, uint32_t& freq)
+  {
+    if (bkt_iter_ == (uint32_t)-1)
+      return false;
+
+    char* pBkt = entry_[bkt_iter_];
+    if (pBkt == NULL)
+      return false;
+
+    
+    term = std::string(pBkt+iter_+sizeof(LenType));
+    iter_ += sizeof(LenType) + term.length()+1;
+    freq = *(uint32_t*)(pBkt+iter_);
+    iter_ += sizeof(uint32_t);
+
+    if (iter_ < *((uint32_t*)pBkt+1))
+      return true;
+
+    for (uint32_t i=bkt_iter_+1; i<ENTRY_SIZE; ++i)
+      if (entry_[i]!=NULL)
+      {
+        bkt_iter_ = i;
+        iter_ = 2*sizeof (uint32_t);
+        return true;
+      }
+
+    bkt_iter_ = -1;
+    return false;
+  }
+  
+};
+
+NS_IZENELIB_IR_END
+
+#endif
