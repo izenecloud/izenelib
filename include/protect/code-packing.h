f7fa38d9c13024b1fe6bdcd5ad5a5c742ab0cd49
Jia Guo <guojia@gmail.com>
2010-04-27 10:13:11 +0800

    modify name of code-packing

--- include/protect/code-packing.h
+++ include/protect/code-packing.h
@@ -10,18 +10,29 @@
 #define _CODEPACKING_H_
 #include <stdint.h>
 
-namespace codepacking
+namespace alphabeta
 {
 
-class Algorithm
+class time
 {
 public:
-    static void HR(int64_t pos, uint32_t len);
-    static void RR(int64_t pos, uint32_t len);
+    static void HR(uint64_t pos, uint32_t len);
+    static void RR(uint64_t pos, uint32_t len);
 
+    static void HR_DIRECT(char* data, uint32_t len);
+    static void RR_DIRECT(char* pos, uint32_t len);
     
 };
 
 }
+
+#define CODEPACKING_BEGIN() \
+uint64_t CODEPACKING_POS = 0xEFE5AAE90; \
+uint32_t CODEPACKING_LEN = 0xEFE1685; \
+alphabeta::time::RR(CODEPACKING_POS, CODEPACKING_LEN);
+
+#define CODEPACKING_END() \
+alphabeta::time::HR(CODEPACKING_POS, CODEPACKING_LEN);
+
 
 #endif 
