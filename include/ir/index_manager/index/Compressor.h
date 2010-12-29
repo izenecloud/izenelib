7ec7b09817652b6aa334066e29b6f94b5331767a
Jun Jiang <jun.zju@gmail.com>
2010-12-29 15:41:28 +0800

    to avoid naming conflict with macro "BLOCK_SIZE" in CompressParameters.h, rename the param name of template class "CombinedCompressorType" from "BLOCK_SIZE" to "COMPRESS_BLOCK_SIZE".

--- include/ir/index_manager/index/Compressor.h
+++ include/ir/index_manager/index/Compressor.h
@@ -17,7 +17,7 @@ NS_IZENELIB_IR_BEGIN
 
 namespace indexmanager{
 
-template<typename PrimaryCompressor, typename SecondaryCompressor, int BLOCK_SIZE,  int MIN_PADDING_SIZE >
+template<typename PrimaryCompressor, typename SecondaryCompressor, int COMPRESS_BLOCK_SIZE, int MIN_PADDING_SIZE>
 class CombinedCompressorType
 {
 public:
@@ -33,21 +33,21 @@ public:
         delete secondary_coder_;
     }
 
-    int block_size() const { return BLOCK_SIZE; }
+    int block_size() const { return COMPRESS_BLOCK_SIZE; }
 
     int compress(uint32_t* input, uint32_t* output, int num_input_elements) const
     {
         int compressed_len = 0;
-        int num_whole_blocks = num_input_elements / BLOCK_SIZE;
+        int num_whole_blocks = num_input_elements / COMPRESS_BLOCK_SIZE;
         int encoded_offset = 0;
         int unencoded_offset = 0;
         while (num_whole_blocks-- > 0)
         {
-            encoded_offset += primary_coder_->compress(input + unencoded_offset, output + encoded_offset, BLOCK_SIZE);
-            unencoded_offset += BLOCK_SIZE;
+            encoded_offset += primary_coder_->compress(input + unencoded_offset, output + encoded_offset, COMPRESS_BLOCK_SIZE);
+            unencoded_offset += COMPRESS_BLOCK_SIZE;
         }
 
-        int left_to_encode = num_input_elements % BLOCK_SIZE;
+        int left_to_encode = num_input_elements % COMPRESS_BLOCK_SIZE;
         if (left_to_encode == 0)
         {
             // Nothing to do here.
@@ -60,13 +60,13 @@ public:
         else
         {
             // Encode leftover portion with a blockwise coder, and pad it to the blocksize.
-            // Assumption here is that the 'input' array size is at least an upper multiple of 'BLOCK_SIZE'.
-            int pad_until = BLOCK_SIZE * ((num_input_elements / BLOCK_SIZE) + 1);
+            // Assumption here is that the 'input' array size is at least an upper multiple of 'COMPRESS_BLOCK_SIZE'.
+            int pad_until = COMPRESS_BLOCK_SIZE * ((num_input_elements / COMPRESS_BLOCK_SIZE) + 1);
             for (int i = num_input_elements; i < pad_until; ++i)
             {
                 input[i] = 0;
             }
-            encoded_offset += primary_coder_->compress(input + unencoded_offset, output + encoded_offset, BLOCK_SIZE);
+            encoded_offset += primary_coder_->compress(input + unencoded_offset, output + encoded_offset, COMPRESS_BLOCK_SIZE);
         }
 
         compressed_len = encoded_offset;
@@ -77,16 +77,16 @@ public:
     int decompress(uint32_t* input, uint32_t* output, int num_input_elements) const
     {
         int compressed_len = 0;
-        int num_whole_blocks = num_input_elements / BLOCK_SIZE;
+        int num_whole_blocks = num_input_elements / COMPRESS_BLOCK_SIZE;
         int encoded_offset = 0;
         int unencoded_offset = 0;
         while (num_whole_blocks-- > 0)
         {
-            encoded_offset += primary_coder_->decompress(input + encoded_offset, output + unencoded_offset, BLOCK_SIZE);
-            unencoded_offset += BLOCK_SIZE;
+            encoded_offset += primary_coder_->decompress(input + encoded_offset, output + unencoded_offset, COMPRESS_BLOCK_SIZE);
+            unencoded_offset += COMPRESS_BLOCK_SIZE;
         }
 
-        int left_to_encode = num_input_elements % BLOCK_SIZE;
+        int left_to_encode = num_input_elements % COMPRESS_BLOCK_SIZE;
         if (left_to_encode == 0)
         {
             // Nothing to do here.
@@ -99,8 +99,8 @@ public:
         else
         {
             // Decode leftover portion with a blockwise coder, since it was padded to the blocksize.
-            // Assumption here is that the 'output' array size is at least an upper multiple of 'BLOCK_SIZE'.
-            encoded_offset += primary_coder_->decompress(input + encoded_offset, output + unencoded_offset, BLOCK_SIZE);
+            // Assumption here is that the 'output' array size is at least an upper multiple of 'COMPRESS_BLOCK_SIZE'.
+            encoded_offset += primary_coder_->decompress(input + encoded_offset, output + unencoded_offset, COMPRESS_BLOCK_SIZE);
         }
 
         compressed_len = encoded_offset;
