75a582bb5d229af5c62d03c99b5e910186de4fc4
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-01-31 18:20:49 +0800

    Add an utility to load dynamic library cross platform

---
+++ include/util/DynamicLibrary.h
@@ -0,0 +1,141 @@
+/*
+ *  Copyright 2009 Utkin Dmitry
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+ 
+#ifndef _DYNAMICLIBRARY_H_
+#define _DYNAMICLIBRARY_H_
+
+#ifdef WIN32
+#include <windows.h>
+
+#pragma warning (disable : 4251) // needs to have dll-interface to be used by clients of class rise...
+
+#define IZENE_DLL_EXPORT __declspec(dllexport)
+#define IZENE_DLL_IMPORT __declspec(dllimport)
+
+#ifdef IZENE_DLL_EXPORTS
+#define IZENE_EXPORT IZENE_DLL_EXPORT
+#else
+#define IZENE_EXPORT IZENE_DLL_IMPORT
+#endif
+
+#define IZENE_LIBRARY_PREFIX ""
+#define IZENE_LIBRARY_EXT ".dll"
+
+typedef HMODULE HDynamicLib;
+
+#else
+#define IZENE_EXPORT 
+#define IZENE_DLL_EXPORT
+#define IZENE_DLL_IMPORT
+
+#ifdef OS_Darwin
+#define IZENE_LIBRARY_PREFIX "lib"
+#define IZENE_LIBRARY_EXT ".dylib"
+#else
+#define IZENE_LIBRARY_PREFIX "lib"
+#define IZENE_LIBRARY_EXT ".so"
+#endif
+
+#include <dlfcn.h>
+//! dynamic library handle
+typedef void* HDynamicLib;
+#endif
+
+#include <string>
+
+
+namespace izenelib{ namespace util {
+
+
+//! library symbol
+typedef void* PLibSymbol;
+
+/* load library
+*   param  sLibraryName - library name
+*   param  bRawName - use system-specific library name
+*   return dynamic library handle, NULL if error
+*/
+HDynamicLib osLoadLibrary(const std::string& sLibraryName, bool bRawName);
+
+/* get library symbol
+*  param  hDynamicLib - dynamic lib handle
+*  param  sSymName - symbol name
+*  return pointer to symbol, NULL if error
+*/
+PLibSymbol osGetSymbol(HDynamicLib hDynamicLib, const std::string& sSymName);
+
+/* unload and free library
+*  param  hDynamicLib - dynamic lib handle
+*  return true, if library was successfully unloaded
+*/
+bool osFreeLibrary(HDynamicLib hDynamicLib);
+
+/* get error description
+* param	nError - error code
+* return error description
+*/
+IZENE_EXPORT std::string osGetErrorStr( int nError );
+
+/* get last error code
+*  return last error code
+*/
+long osGetLastLibraryError();
+
+/* get last error description
+*   return last error description
+*/
+std::string osGetLastLibraryErrorStr();
+
+
+
+class IZENE_EXPORT DynamicLibrary
+{
+public:
+
+    DynamicLibrary();
+
+    virtual ~DynamicLibrary();
+      
+    /* load dynamic library
+    *   param  sLibName - library name
+    *   param  bRawName - true, given full library name with extension
+    */
+    virtual void load(const std::string& sLibName, bool bRawName = false);
+
+    /* get library name
+    *   return library name
+    */
+    const std::string& getName() const;
+
+    /* get library symbol pointer
+    *   param  sSymName - symbol name
+    *   return pointer to symbol
+    */
+    PLibSymbol getSymbol(const std::string& sSymName) const;
+
+    /* unload library
+    */
+    void unloadLibrary();
+
+private:
+    HDynamicLib hDynLib_; //!< library handle
+    std::string name_;   //!< library name
+};
+
+  }}
+
+#endif // _DYNAMICLIBRARY_H_
+
---
+++ source/util/DynamicLibrary.cpp
@@ -0,0 +1,143 @@
+#include <util/DynamicLibrary.h>
+
+#include <stdexcept>
+
+namespace izenelib{ namespace util{
+
+HDynamicLib osLoadLibrary( const std::string& sLibraryName, bool bRawName )
+{
+#ifdef WIN32
+    if(bRawName)
+      return LoadLibrary(sLibraryName.c_str());
+    else
+      return LoadLibrary((sLibraryName + IZENE_LIBRARY_EXT).c_str());
+#else
+    if(bRawName)
+        return dlopen(sLibraryName.c_str(), RTLD_LAZY);
+    else
+    {
+        std::string::size_type nPos = sLibraryName.find_last_of('/');
+        if(nPos == std::string::npos)
+            return dlopen((IZENE_LIBRARY_PREFIX + sLibraryName + IZENE_LIBRARY_EXT).c_str(), RTLD_LAZY);
+        else
+            return dlopen((sLibraryName.substr(nPos) + IZENE_LIBRARY_PREFIX + sLibraryName.substr(nPos + 1) + IZENE_LIBRARY_EXT).c_str(), RTLD_LAZY);
+    }
+#endif
+}
+
+PLibSymbol osGetSymbol( HDynamicLib hDynamicLib, const std::string& sSymName )
+{
+    return 
+#ifdef WIN32
+    GetProcAddress(hDynamicLib, sSymName.c_str());
+#else
+    dlsym(hDynamicLib, sSymName.c_str());
+#endif
+}
+
+bool osFreeLibrary(HDynamicLib hDynamicLib)
+{
+    return 
+#ifdef WIN32
+    FreeLibrary(hDynamicLib) == TRUE;
+#else
+    dlclose(hDynamicLib);
+#endif
+}
+
+IZENE_EXPORT std::string osGetErrorStr( int nError )
+{
+#ifdef WIN32
+    std::string sMsg;
+
+    sMsg.resize(1024);
+
+    DWORD dwChars = ::FormatMessage( FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+              0, nError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), 
+              const_cast<TChar*>(sMsg.c_str()), static_cast<DWORD>(sMsg.size()), NULL );
+
+    if (dwChars == 0)
+        return "<Unknown error>";
+
+    sMsg.erase(dwChars);
+
+    return sMsg;
+#else
+    return strerror(nError);
+#endif
+}
+
+
+long osGetLastLibraryError()
+{
+    return 
+#ifdef WIN32
+    GetLastError();
+#else
+    dlerror() == NULL ? -1 : 0;
+#endif
+}
+
+std::string osGetLastLibraryErrorStr()
+{
+    return 
+#ifdef WIN32
+    osGetErrorStr(osGetLastLibraryError());
+#else
+    dlerror();
+#endif
+}
+
+//////////////////////////////////////////////////////
+//
+//                      DynamicLibrary
+//
+/////////////////////////////////////////////////////
+
+DynamicLibrary::DynamicLibrary()
+    :hDynLib_(NULL)
+{}
+
+DynamicLibrary::~DynamicLibrary()
+{
+    try 
+    {
+        if ( hDynLib_ )
+        {
+            unloadLibrary();
+        }
+    } catch (...) {}
+}
+
+void DynamicLibrary::load( const std::string& sLibName, bool bRawName /*= false*/ )
+{
+    if(!hDynLib_) throw std::runtime_error("library has already been initialized");	
+    hDynLib_ = osLoadLibrary(sLibName, bRawName);
+    if(!hDynLib_) throw std::runtime_error("library load exception:"+osGetLastLibraryErrorStr());	
+    name_ = sLibName;
+}
+
+const std::string& DynamicLibrary::getName() const
+{
+    if(!hDynLib_) throw std::runtime_error("library not initialized");
+    return name_;
+}
+
+PLibSymbol DynamicLibrary::getSymbol(const std::string& sSymName) const
+{
+    PLibSymbol pSym;
+    if(!hDynLib_) throw std::runtime_error("library not initialized");
+    pSym = osGetSymbol(hDynLib_, sSymName);
+    if(!pSym) throw std::runtime_error("no item named:"+sSymName);
+    return pSym;
+}
+
+void DynamicLibrary::unloadLibrary()
+{
+    if(!hDynLib_) throw std::runtime_error("library not initialized");
+    osFreeLibrary(hDynLib_);
+    hDynLib_ = NULL;
+}
+
+}}
+
