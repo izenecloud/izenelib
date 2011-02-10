55c8abb4716701698142e7fdff9f4d8db3210c32
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-02-10 13:04:22 +0800

    finish major unit tests of osgi

--- test/util/osgi/LauncherTest.cpp
+++ test/util/osgi/LauncherTest.cpp
@@ -23,7 +23,6 @@ BOOST_AUTO_TEST_SUITE(Laucher_test)
 /**
  * Tests whether it is possible to load bundles locally and from library.
  */
-/*
 BOOST_AUTO_TEST_CASE(load_test)
 {
     BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
@@ -35,7 +34,7 @@ BOOST_AUTO_TEST_CASE(load_test)
     Launcher<SingleThreaded,LibraryCreator> launcher;
     launcher.start( bundleConfVec );
 }
-*/
+
 /**
  * Tests whether service listeners can be registered.
  */
@@ -57,68 +56,15 @@ BOOST_AUTO_TEST_CASE(register_test)
     BOOST_CHECK(TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "ServiceA" ) == 1);
 }
 
-#if 0
 
-/**
- * Tests whether services can be used.
- */
-BOOST_AUTO_TEST_CASE(usedservice_test)
-{
-    // Registers service listener for 'ServiceB'
-    // Registers 'ServiceA' with properties 'instance=1'
-    // Registers 'ServiceA' with properties 'instance=2'
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
-
-    // Registers service listener for 'ServiceA'
-    // Registers 'ServiceB' with properties 'instance=1'
-    BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
-
-    vector<BundleConfiguration> bundleConfVec;
-    bundleConfVec.push_back( bundleConf1 );
-    bundleConfVec.push_back( bundleConf2 );
-
-    Launcher<SingleThreaded,LibraryCreator>  launcher;
-    launcher.start( bundleConfVec );
-
-    IRegistry& registry = launcher.getRegistry();
-
-    BOOST_CHECK(TestHelper::isServiceUsedByBundle( registry, "bundle1", "ServiceB", 1 ) == 1);
-
-    // The bundle 'bundle1' registers two services of name 'ServiceA' but
-    // 'bundle2' only uses one of them.
-    BOOST_CHECK(TestHelper::isServiceUsedByBundle( registry, "bundle2", "ServiceA", 1 ) == 1);
-}
-
-/**
- * Tests whether services can be registered.
- */
-BOOST_AUTO_TEST_CASE(registerservice_test)
-{
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
-    BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
-    vector<BundleConfiguration> bundleConfVec;
-    bundleConfVec.push_back( bundleConf1 );
-    bundleConfVec.push_back( bundleConf2 );
-
-    Launcher<SingleThreaded,LibraryCreator>  launcher;
-    launcher.start( bundleConfVec );
-
-    IRegistry& registry = launcher.getRegistry();
-
-    BOOST_CHECK( TestHelper::isServiceRegisteredByBundle( registry, "bundle1", "ServiceA", 1 ) == 2);
-
-    BOOST_CHECK( TestHelper::isServiceRegisteredByBundle( registry, "bundle2", "ServiceB", 1 ) == 1);
-}
 
 /**
  * Tests whether services can be deregistered.
  */
 BOOST_AUTO_TEST_CASE(unregisterservice_test)
 {
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
     BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
     vector<BundleConfiguration> bundleConfVec;
-    bundleConfVec.push_back( bundleConf1 );
     bundleConfVec.push_back( bundleConf2 );
 
     Launcher<SingleThreaded,LibraryCreator>  launcher;
@@ -129,12 +75,6 @@ BOOST_AUTO_TEST_CASE(unregisterservice_test)
     IRegistry& registry = launcher.getRegistry();
     BOOST_CHECK( TestHelper::isServiceRegisteredByBundle( registry, "bundle2", "ServiceB", 1 ) == 0);
 
-    BOOST_CHECK( TestHelper::isServiceUsedByBundle( registry, "bundle1", "ServiceB", 1 ) == 0);
-
-    BOOST_CHECK( TestHelper::isServiceRegisteredByBundle( registry, "bundle1", "ServiceA", 1 ) == 2);
-
-    BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle1", "ServiceB" ) == 1);
-
     BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "ServiceA" ) == 1);
 }
 
@@ -143,14 +83,11 @@ BOOST_AUTO_TEST_CASE(unregisterservice_test)
  */
 BOOST_AUTO_TEST_CASE(unregisterlistener_test)
 {
-    // Registers service listener for 'ServiceB'
-    // Registers 'ServiceA' with properties 'instance=1'
-    // Registers 'ServiceA' with properties 'instance=2'
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
+    // Registers 'IMultiplier' with properties 'instance=1'
+    BundleConfiguration bundleConf1( "bundle1", "BundleActivator1", ".", "libbundle1.so" );
 
-    // Registers service listener for 'ServiceA'
-    // Registers 'ServiceB' with properties 'instance=1'
-    BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
+    // Registers service listener for 'IMultiplier'
+    BundleConfiguration bundleConf2( "bundle2", "BundleActivator2", ".", "libbundle2.so" );
 
     vector<BundleConfiguration> bundleConfVec;
     bundleConfVec.push_back( bundleConf1 );
@@ -161,43 +98,21 @@ BOOST_AUTO_TEST_CASE(unregisterlistener_test)
 
     IRegistry& registry = launcher.getRegistry();
 
-    BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "ServiceA" ) == 1);
+    BOOST_CHECK(TestHelper::isServiceRegisteredByBundle( registry, "bundle1", "IMultiplier", 1 ) == 1);
+
+    BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "IMultiplier" ) == 1);
 
     // Stops the service tracker
-    TestBundleActivator::stopServiceListener();
+    ///TODO
+    //TestBundleActivator::stopServiceListener();
 
-    BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "ServiceA" ) == 0);
-}
-
-/**
- * Tests whether a bundle can be stopped.
- */
-BOOST_AUTO_TEST_CASE(stop_test)
-{
-
-    // Registers service listener for 'ServiceB'
-    // Registers 'ServiceA' with properties 'instance=1'
-    // Registers 'ServiceA' with properties 'instance=2'
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
-
-    // Registers service listener for 'ServiceA'
-    // Registers 'ServiceB' with properties 'instance=1'
-    BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
-
-    vector<BundleConfiguration> bundleConfVec;
-    bundleConfVec.push_back( bundleConf1 );
-    bundleConfVec.push_back( bundleConf2 );
-
-    Launcher<SingleThreaded,LibraryCreator>  launcher;
-    launcher.start( bundleConfVec );
-
-    IRegistry& registry = launcher.getRegistry();
-
+    //BOOST_CHECK( TestHelper::isServiceListenerRegisteredByBundle( registry, "bundle2", "IMultiplier" ) == 0);
     launcher.stopBundle( "bundle2" );
 
     BOOST_CHECK( TestHelper::isBundleStarted( registry, "bundle2" ) == false );
 
-    BOOST_CHECK( TestHelper::isServiceUsedByBundle( registry, "bundle1", "ServiceB", 1 ) == 0 );
+    BOOST_CHECK( TestHelper::isServiceUsedByBundle( registry, "bundle1", "IMultiplier", 1 ) == 0 );
+    
 }
 
 /**
@@ -205,7 +120,7 @@ BOOST_AUTO_TEST_CASE(stop_test)
  */
 BOOST_AUTO_TEST_CASE(stopall_test)
 {
-    BundleConfiguration bundleConf1( "bundle1", "BundleActivator2", ".", "libbundle2.so" );
+    BundleConfiguration bundleConf1( "bundle1", "BundleActivator1", ".", "libbundle1.so" );
     BundleConfiguration bundleConf2( "bundle2", "TestBundleActivator" );
     vector<BundleConfiguration> bundleConfVec;
     bundleConfVec.push_back( bundleConf1 );
@@ -222,6 +137,6 @@ BOOST_AUTO_TEST_CASE(stopall_test)
 
     BOOST_CHECK( TestHelper::isBundleStarted( registry, "bundle1" ) == false);
 }
-#endif
+
 BOOST_AUTO_TEST_SUITE_END()
 
