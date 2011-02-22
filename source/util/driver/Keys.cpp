c6a3a18db31094005431d3df701c2cd86f02d5d7
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-02-22 11:09:03 +0800

    driver keys' definition

--- source/util/driver/Keys.cpp
+++ source/util/driver/Keys.cpp
@@ -9,15 +9,15 @@
 namespace izenelib {
 namespace driver {
 
-#define SF1_DRIVER_KEYS_DEF(z,i,l) \
+#define IZENELIB_DRIVER_KEYS_DEF(z,i,l) \
     const std::string Keys::BOOST_PP_SEQ_ELEM(i,l) = \
         BOOST_PP_STRINGIZE(BOOST_PP_SEQ_ELEM(i,l));
 
 BOOST_PP_REPEAT(
-    BOOST_PP_SEQ_SIZE(SF1_DRIVER_KEYS),
-    SF1_DRIVER_KEYS_DEF,
-    SF1_DRIVER_KEYS
+    BOOST_PP_SEQ_SIZE(IZENELIB_DRIVER_KEYS),
+    IZENELIB_DRIVER_KEYS_DEF,
+    IZENELIB_DRIVER_KEYS
 )
 
-#undef SF1_DRIVER_KEYS_DEF
+#undef IZENELIB_DRIVER_KEYS_DEF
 }} // namespace izenelib::driver
