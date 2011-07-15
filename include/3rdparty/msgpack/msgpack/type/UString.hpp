4dfe6d31667c051abeacc08fa86924ab789cd043
Zhongxia Li <zhongxia.li2010@gmail.com>
2011-07-15 18:19:53 +0800

    avoid ambiguous of ustring namespace in msgpack.

--- include/3rdparty/msgpack/msgpack/type/UString.hpp
+++ include/3rdparty/msgpack/msgpack/type/UString.hpp
@@ -6,35 +6,34 @@
 #include <util/ustring/UString.h>
 
 
-using namespace izenelib::util;
 
 namespace msgpack {
 
-inline UString& operator>> (object o, UString& uv)
+inline izenelib::util::UString& operator>> (object o, izenelib::util::UString& uv)
 {
     std::string v;
     if(o.type != type::RAW) { throw type_error(); }
     v.assign(o.via.raw.ptr, o.via.raw.size);
 
-    uv.assign(v, UString::UTF_8); // Fix encoding, by define new object type
+    uv.assign(v, izenelib::util::UString::UTF_8); // Fix encoding, by define new object type
     return uv;
 }
 
 template <typename Stream>
-inline packer<Stream>& operator<< (packer<Stream>& o, const UString& uv)
+inline packer<Stream>& operator<< (packer<Stream>& o, const izenelib::util::UString& uv)
 {
     std::string v;
-    uv.convertString(v, UString::UTF_8);
+    uv.convertString(v, izenelib::util::UString::UTF_8);
 
     o.pack_raw(v.size());
     o.pack_raw_body(v.data(), v.size());
     return o;
 }
 
-inline void operator<< (object::with_zone& o, const UString& uv)
+inline void operator<< (object::with_zone& o, const izenelib::util::UString& uv)
 {
     std::string v;
-    uv.convertString(v, UString::UTF_8);
+    uv.convertString(v, izenelib::util::UString::UTF_8);
 
     o.type = type::RAW;
     char* ptr = (char*)o.zone->malloc(v.size());
@@ -43,10 +42,10 @@ inline void operator<< (object::with_zone& o, const UString& uv)
     memcpy(ptr, v.data(), v.size());
 }
 
-inline void operator<< (object& o, const UString& uv)
+inline void operator<< (object& o, const izenelib::util::UString& uv)
 {
     std::string v;
-    uv.convertString(v, UString::UTF_8);
+    uv.convertString(v, izenelib::util::UString::UTF_8);
 
     o.type = type::RAW;
     o.via.raw.ptr = v.data();
