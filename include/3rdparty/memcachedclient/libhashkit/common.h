3cbb6cbc52ad34dc8652c37fcdd9d2d01607cd19
Zhongxia Li <zhongxia.li2010@gmail.com>
2011-09-19 19:47:54 +0800

    add libMemcached which is a memcached client library.

---
+++ include/3rdparty/memcachedclient/libhashkit/common.h
@@ -0,0 +1,33 @@
+/* HashKit
+ * Copyright (C) 2009 Brian Aker
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license.  See
+ * the COPYING file in the parent directory for full text.
+ */
+
+#pragma once
+
+#include <config.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <libhashkit/hashkit.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HASHKIT_LOCAL
+void md5_signature(const unsigned char *key, unsigned int length, unsigned char *result);
+
+HASHKIT_LOCAL
+int update_continuum(hashkit_st *hashkit);
+
+#ifdef __cplusplus
+}
+#endif
