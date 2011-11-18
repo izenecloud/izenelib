b28c08730a246d0e454bca70727df7e23763cda0
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-11-18 20:43:12 +0800

    improve compressed_vector(unordered_vector has not been fully correct yet)

--- test/util/compressed_vector/t_compressed_vector.cpp
+++ test/util/compressed_vector/t_compressed_vector.cpp
@@ -2,7 +2,7 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <util/compressed_vector/OrderedVector.h>
+#include <util/compressed_vector/VIntVector.h>
 
 #include <string>
 #include <iostream>
@@ -13,36 +13,62 @@ using namespace std;
 
 
 BOOST_AUTO_TEST_SUITE(Compressed_vector_test)
+
 BOOST_AUTO_TEST_CASE(ordered_vector)
 {
     MemPool memPool(128);
-    izenelib::util::compressed_vector::OrderedVector v(&memPool);
+    izenelib::util::compressed_vector::OrderedVIntVector v(&memPool);
 
     size_t count = 1000;
     size_t i = 1;
-    for(; i < count; ++i)
-        v.push_back(i);
+    size_t magic = 3;
+    for(; i <= count; ++i)
+        v.push_back(i*magic);
 
     BOOST_CHECK(v.size() == count);
 
-    typedef izenelib::util::compressed_vector::OrderedVector::iterator IteratorType;
+    typedef izenelib::util::compressed_vector::OrderedVIntVector::iterator IteratorType;
     IteratorType iter = v.begin();
     IteratorType end = v.end();
     i = 1;
-    for(;iter !=end; ++iter)
+    for(;iter !=end; ++iter,++i)
     {
-        BOOST_CHECK(*iter == (i ++));
+        BOOST_CHECK(*iter == i*magic);
     }
-    const izenelib::util::compressed_vector::OrderedVector& vv(v);
-    typedef izenelib::util::compressed_vector::OrderedVector::const_iterator CIteratorType;
+    const izenelib::util::compressed_vector::OrderedVIntVector& vv(v);
+    typedef izenelib::util::compressed_vector::OrderedVIntVector::const_iterator CIteratorType;
     CIteratorType citer = vv.begin();
     CIteratorType cend = vv.end();
     i = 1;
-    for(;citer !=cend; ++citer)
+    for(;citer !=cend; ++citer,++i)
     {
-        BOOST_CHECK(*citer == (i ++));
+        BOOST_CHECK(*citer == i*magic);
     }
-	
+}
+
+BOOST_AUTO_TEST_CASE(unordered_vector)
+{
+    MemPool memPool(128);
+    izenelib::util::compressed_vector::UnOrderedVIntVector v(&memPool);
+
+    size_t count = 100;
+    size_t i = 1;
+    size_t magic = 3;
+
+    for(; i <=count; ++i)
+        v.push_back(i*magic);
+
+    BOOST_CHECK(v.size() == count);
+
+    typedef izenelib::util::compressed_vector::UnOrderedVIntVector::iterator IteratorType;
+    IteratorType iter = v.begin();
+    IteratorType end = v.end();
+    i = 1;
+    for(;iter !=end; ++iter,++i)
+    {
+        BOOST_CHECK(*iter == i*magic );
+        //std::cout<<*iter<<" "<<i*magic<<std::endl;
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END() // Compressed_vectortest
