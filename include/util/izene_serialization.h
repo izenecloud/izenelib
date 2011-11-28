b580edf9f8e71395fc7231b06573a242581eb9ef
August Njam Grong <longran1989@gmail.com>
2011-11-28 13:45:10 +0800

    tweak unit test for am/tc

--- include/util/izene_serialization.h
+++ include/util/izene_serialization.h
@@ -4,7 +4,7 @@
  * @author Peisheng Wang
  *
  * This file defines clas izene_serialization and izene_deserialization.
- * 
+ *
  * @history
  *  - 2009.7.27 Peisheng Wnag
  *
@@ -24,72 +24,72 @@ NS_IZENELIB_UTIL_BEGIN
 template<typename T, bool isMemcpy = false, bool isFeBird = false>
 struct izene_serial_type
 {
-	typedef izene_serialization_boost<T> stype;
-	typedef izene_deserialization_boost<T> dtype;
+    typedef izene_serialization_boost<T> stype;
+    typedef izene_deserialization_boost<T> dtype;
 };
 
 template<typename T>
 struct izene_serial_type<T, true, false>
 {
-	typedef izene_serialization_memcpy<T> stype;
-	typedef izene_deserialization_memcpy<T> dtype;
+    typedef izene_serialization_memcpy<T> stype;
+    typedef izene_deserialization_memcpy<T> dtype;
 };
 
 template<typename T>
 struct izene_serial_type<T, true, true>
 {
-	typedef izene_serialization_memcpy<T> stype;
-	typedef izene_deserialization_memcpy<T> dtype;
+    typedef izene_serialization_memcpy<T> stype;
+    typedef izene_deserialization_memcpy<T> dtype;
 };
 
 template<typename T>
 struct izene_serial_type<T, false, true>
 {
-	typedef izene_serialization_febird<T> stype;
-	typedef izene_deserialization_febird<T> dtype;
+    typedef izene_serialization_febird<T> stype;
+    typedef izene_deserialization_febird<T> dtype;
 };
 
 
 /**
- * 
- *  \brief izene_serialization 
- *  
- *  It wraps boost, febird and memory serialization into one unified interface according to the type 
- *  
+ *
+ *  \brief izene_serialization
+ *
+ *  It wraps boost, febird and memory serialization into one unified interface according to the type
+ *
  */
 template <typename T> class izene_serialization {
-	typedef typename izene_serial_type< T, IsMemcpySerial<T>::yes, IsFebirdSerial<T>::yes>::stype
-			stype;
-	stype impl;
+    typedef typename izene_serial_type< T, IsMemcpySerial<T>::yes, IsFebirdSerial<T>::yes>::stype
+            stype;
+    stype impl;
 public:
-	izene_serialization(const T& dat) :
-		impl(dat) {
+    izene_serialization(const T& dat) :
+        impl(dat) {
 
-	}
-	void write_image(char* &ptr, size_t &size) {
-			impl.write_image(ptr, size);
-		}
+    }
+    void write_image(char* &ptr, size_t &size) {
+            impl.write_image(ptr, size);
+        }
 };
 
 
 /**
- * 
- *  \brief izene_deserialization 
- * 
+ *
+ *  \brief izene_deserialization
+ *
  *  It wraps boost, febird and memory deserialization into one unified interface according to the type
- * 
+ *
  **/
 template <typename T> class izene_deserialization {
-	typedef typename izene_serial_type<T, IsMemcpySerial<T>::yes, IsFebirdSerial<T>::yes>::dtype
-			dtype;
-	dtype impl;
+    typedef typename izene_serial_type<T, IsMemcpySerial<T>::yes, IsFebirdSerial<T>::yes>::dtype
+            dtype;
+    dtype impl;
 public:
-	izene_deserialization(const char* ptr, const size_t size) :
-		impl(ptr, size) {
-	}
-	void read_image(T& dat) {
-		impl.read_image(dat);
-	}
+    izene_deserialization(const char* ptr, const size_t size) :
+        impl(ptr, size) {
+    }
+    void read_image(T& dat) {
+        impl.read_image(dat);
+    }
 };
 
 
