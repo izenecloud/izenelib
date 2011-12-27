a166830e43c8f562dbf68c7d9a1d5abf4bb771f9
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-12-27 12:16:15 +0800

    remove verbose outputs for t_util

--- test/util/t_bzip.cpp
+++ test/util/t_bzip.cpp
@@ -9,31 +9,20 @@ using namespace std;
 
 BOOST_AUTO_TEST_CASE(bzip_test)
 {
-
-	const char *input = "abcd34252dfjsfljt23lsj lfserrrrrrrrrrrrrrrrrr111111111111111111111111111111111111111111111111111111 \
+    const char *input = "abcd34252dfjsfljt23lsj lfserrrrrrrrrrrrrrrrrr111111111111111111111111111111111111111111111111111111 \
 			11111111111111111111111111111111111111111111111111111111111111111111111111111111112u4o2u42j5l2j35l2jhl5ljljrl23525";
-			
-	int isz = strlen(input);
+    int isz = strlen(input);
+    char *output;
+    int osz;
 
-	char *output;
-	int osz;
+    char *output1;
+    int osz1;
 
-	char *output1;
-	int osz1;
+    output = _tc_bzcompress(input, isz, &osz);
 
-	cout<<"input: \n"<<input<<endl;
-	cout<<"sz: \n"<<isz<<endl;
-	output = _tc_bzcompress(input, isz, &osz);
-	cout<<"output: \n"<<output<<endl;
-	cout<<"sz: \n"<<osz<<endl;
+    output1 = _tc_bzdecompress(output, osz, &osz1);
 
-	output1 = _tc_bzdecompress(output, osz, &osz1);
-	cout<<"output1: \n"<<output1<<endl;
-	cout<<"sz1: \n"<<osz1<<endl;
-	
-	
-	BOOST_CHECK(isz == osz1);
-	free(output);
-	free(output1);
-
+    BOOST_CHECK(isz == osz1);
+    free(output);
+    free(output1);
 }
