d83ee37b51f240b44b9c1e84c1fb60da6e3c150f
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2012-06-08 14:14:36 +0800

    rm babudb

--- include/3rdparty/am/babudb/babudb.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2008, Felix Hupfeld, Jan Stender, Bjoern Kolbeck, Mikael Hoegqvist, Zuse Institute Berlin.
-// Licensed under the BSD License, see LICENSE file for details.
-
-#include "database.h"
-#include "key.h"
-#include "buffer.h"
-#include "log/log_iterator.h"
-#include "lookup_iterator.h"
