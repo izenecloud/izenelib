db37fc7f5ba38b2aa7b06ba20aa5aae2d331e164
August Njam Grong <longran1989@gmail.com>
2012-09-04 20:55:42 +0800

    tiny

--- include/am/succinct/fm-index/wavelet_tree_huffman.hpp
+++ include/am/succinct/fm-index/wavelet_tree_huffman.hpp
@@ -308,7 +308,7 @@ size_t WaveletTreeHuffman<CharT>::select(char_type c, size_t rank) const
 
     for (; walk->parent_; walk = walk->parent_)
     {
-        if ((rank = walk->bit_vector_.Select(rank, bit)) == -1)
+        if ((rank = walk->bit_vector_.Select(rank, bit)) == (size_t)-1)
             return -1;
 
         bit = (walk == walk->parent_->right_);
