e9b4e6c9d54e1ae4d6b56e663f72e9d15bf386dc
Vincent Lee <vincent.lee@b5m.com>
2013-07-16 19:03:52 +0800

    tiny fix

--- include/ir/ir_database/IRDatabase.hpp
+++ include/ir/ir_database/IRDatabase.hpp
@@ -678,6 +678,7 @@ namespace irdb
                     
                     indexer_->flush();
                     indexer_->optimizeIndex();
+                    indexer_->waitForMergeFinish();
                     DBHandlerFlush<DATA_VECTOR_TYPE, DB_VECTOR_TYPE, boost::mpl::size<DATA_VECTOR_TYPE>::value-1>::act(*this);
 
                     if(serInfo_)
