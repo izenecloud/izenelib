9cf19f963494f97ce8cb9d4c0f65ceeb0c5cc871
Hongliang <hongliang.zhao@b5m.com>
2013-09-06 09:39:24 +0800

    tiny

--- test/ir/Zambezi/newInvertedIndexTestFixture.h
+++ test/ir/Zambezi/newInvertedIndexTestFixture.h
@@ -222,7 +222,9 @@ namespace Zambezi
         {
             std::vector<uint32_t> score_list;
             uint32_t hits = 10000000;
+            Algorithm algorithm = SVS;
             index_->retrieval(
+                algorithm,
                 term_list,
                 hits,
                 docid_list,
