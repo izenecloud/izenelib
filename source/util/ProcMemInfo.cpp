088f9580fee148b7a9464b7f0ea38e494f0cbd36
Jun Jiang <jun.jiang@izenesoft.com>
2013-10-31 14:13:01 +0800

    Fix a bug in ProcMemInfo.
    This bug was brought in 3e85cbc3cb1fe67b2476d93638d354433d92d31a,
    which leads to infinite loop.

--- source/util/ProcMemInfo.cpp
+++ source/util/ProcMemInfo.cpp
@@ -5,28 +5,29 @@ NS_IZENELIB_UTIL_BEGIN
 void ProcMemInfo::getProcMemInfo(unsigned long & virtualMem, unsigned long & realMem)
     throw (ios_base::failure)
 {
-    char temp[50] = {0,};
-    string buffer;
-    sprintf( temp, "/proc/%d/statm", getpid() );
-
-    try{        
-        getStatFile(temp, buffer);
-        readProcStatus(buffer, virtualMem, realMem);
-
-        realMem = realMem << 12;    //each page is 4K, so multiply by 4
-        virtualMem = virtualMem << 12;    //each page is 4K, so multiply by 4
-    }
-    catch (ios_base::failure e)
-    {
-        throw e;
-    }
-
+    getProcMemInfo(getpid(), virtualMem, realMem);
 }
 
 void ProcMemInfo::getProcMemInfo(pid_t pid, unsigned long & virtualMem, unsigned long & realMem)
     throw (ios_base::failure)
 {
-    getProcMemInfo(getpid(), virtualMem, realMem);
+    char temp[50] = {0,};
+    string buffer;
+    sprintf(temp, "/proc/%d/statm", pid);
+
+    try
+    {
+        getStatFile(temp, buffer);
+        readProcStatus(buffer, virtualMem, realMem);
+
+        // each page is 4K, so multiply by 4K
+        realMem = realMem << 12;
+        virtualMem = virtualMem << 12;
+    }
+    catch (ios_base::failure e)
+    {
+        throw e;
+    }
 }
 
 void ProcMemInfo::getStatFile(const string path, string & buffer)
