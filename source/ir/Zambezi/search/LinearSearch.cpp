94537c77f0439069e5827d739262ff977e7229bc
August Njam Grong <longran1989@gmail.com>
2014-02-25 17:56:01 +0800

    reduce temporary variables

--- source/ir/Zambezi/search/LinearSearch.cpp
+++ source/ir/Zambezi/search/LinearSearch.cpp
@@ -17,38 +17,37 @@ uint32_t linearSearch(
     if (index >= count || LESS_THAN(block[count - 1], pivot, reverse))
         return INVALID_ID;
 
-    const __m128i* data = reinterpret_cast<const __m128i*>(&block[index]);
-    const __m128i* tmp = data;
-    const __m128i pivot4 = _mm_set1_epi32(pivot);
+    __m128i pivot4 = _mm_set1_epi32(pivot);
+    const uint32_t* tmp = block + index;
 
     if (reverse)
     {
-        for (;; tmp += 4)
+        for (;; tmp += 16)
         {
-            const __m128i cmp0 = _mm_cmpgt_epi32(_mm_loadu_si128(tmp), pivot4);
-            const __m128i cmp1 = _mm_cmpgt_epi32(_mm_loadu_si128(tmp + 1), pivot4);
-            const __m128i cmp2 = _mm_cmpgt_epi32(_mm_loadu_si128(tmp + 2), pivot4);
-            const __m128i cmp3 = _mm_cmpgt_epi32(_mm_loadu_si128(tmp + 3), pivot4);
-
             int res = _mm_movemask_epi8(_mm_packs_epi16(
-                        _mm_packs_epi32(cmp0, cmp1), _mm_packs_epi32(cmp2, cmp3)));
+                        _mm_packs_epi32(
+                            _mm_cmpgt_epi32(_mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp)), pivot4),
+                            _mm_cmpgt_epi32(_mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 1), pivot4)),
+                        _mm_packs_epi32(
+                            _mm_cmpgt_epi32(_mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 2), pivot4),
+                            _mm_cmpgt_epi32(_mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 3), pivot4))));
 
-            if (res != 0xffff) return index + (tmp - data) * 4 + __builtin_ctz(~res);
+            if (res != 0xffff) return tmp - block + __builtin_ctz(~res);
         }
     }
     else
     {
-        for (;; tmp += 4)
+        for (;; tmp += 16)
         {
-            const __m128i cmp0 = _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(tmp));
-            const __m128i cmp1 = _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(tmp + 1));
-            const __m128i cmp2 = _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(tmp + 2));
-            const __m128i cmp3 = _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(tmp + 3));
-
             int res = _mm_movemask_epi8(_mm_packs_epi16(
-                        _mm_packs_epi32(cmp0, cmp1), _mm_packs_epi32(cmp2, cmp3)));
+                        _mm_packs_epi32(
+                            _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp))),
+                            _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 1))),
+                        _mm_packs_epi32(
+                            _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 2)),
+                            _mm_cmpgt_epi32(pivot4, _mm_loadu_si128(reinterpret_cast<const __m128i*>(tmp) + 3)))));
 
-            if (res != 0xffff) return index + (tmp - data) * 4 + __builtin_ctz(~res);
+            if (res != 0xffff) return tmp - block + __builtin_ctz(~res);
         }
     }
 
