aa57c1f1d390cf21ffc75354d91c8dea7f02b73f
Vincent Lee <cool8511@gmail.com>
2014-09-16 11:31:39 +0800

    get local host ip fix

--- include/net/distribute/Util.h
+++ include/net/distribute/Util.h
@@ -18,12 +18,13 @@ class Util
 {
 public:
     /// @brief get local host ip
-    static bool getLocalHostIp(std::string& localHostIp)
+    static bool getLocalHostIp(std::string& localHostIp, const std::string& interface = "")
     {
         struct ifaddrs *ifaddr, *ifa;
         int family, s;
         char host[NI_MAXHOST];
 
+        localHostIp.clear();
         if (getifaddrs(&ifaddr) == -1)
         {
            return false;
@@ -46,20 +47,30 @@ public:
                        host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
 
                if (s != 0) {
-                   return false;
+                   ret = false;
+                   break;
                }
 
                // support ipv4 currently, Fixme
                //printf("%s, %s\n", ifa->ifa_name, host);
                if (family == AF_INET)
                {
-                   if ((strncmp(ifa->ifa_name, "eth1", 4) == 0))
+                   if (strncmp(ifa->ifa_name, interface.c_str(), interface.length()))
                    {
                        localHostIp = host;
                        ret = true;
-                       break; // Usually we use LAN address firstly (eth1)
+                       break;
                    }
-                   else if ((strncmp(ifa->ifa_name, "eth0", 4) == 0))
+                   else if ((strncmp(ifa->ifa_name, "eth1", 4) == 0))
+                   {
+                       localHostIp = host;
+                       ret = true;
+                       if (interface.empty())
+                       {
+                           break; // Usually we use LAN address firstly (eth1)
+                       }
+                   }
+                   else if (!ret && (strncmp(ifa->ifa_name, "eth0", 4) == 0))
                    {
                        localHostIp = host;
                        ret = true;
@@ -68,6 +79,7 @@ public:
            }
         }
 
+        freeifaddrs(ifaddr);
         return ret;
     }
 };
